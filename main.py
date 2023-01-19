092693dec9955964f3654fa964f66e25ea885175
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-19 17:50:59 +0300

    First

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 # ------
-x = input("Введите X: ")
-y = input("Введите Y: ")
-a = x+y
-print(a)
-##
+x = int(input("Введите целое число: "))
+y = int(input("Введите целое число: "))
+
+try:
+    x = int(x)
+    y = int(y)
+
+    res = x / y
+except ZeroDivisionError:
+    print("На 0 делить нельзя!")
+except ValueError:
+    print("Одно или несколько значений не число")
+
+else:
+    print("Исключений не произошло")
