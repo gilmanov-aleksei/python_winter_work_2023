1885c707f088eb86c662662e4acb01af2e025c2c
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-19 18:00:21 +0300

    Test_File

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 # ------
-x = int(input("Введите целое число: "))
-y = int(input("Введите целое число: "))
+# Python program to demonstrate
+# quit()
+for i in range(10):
 
-try:
-    x = int(x)
-    y = int(y)
-
-    res = x / y
-except ZeroDivisionError:
-    print("На 0 делить нельзя!")
-except ValueError:
-    print("Одно или несколько значений не число")
-
-else:
-    print("Исключений не произошло")
+    # If the value of i becomes
+    # 5 then the program is forced
+    # to quit
+    if i == 5:
+        # prints the quit message
+        print(quit)
+        quit()
+    print(i)
