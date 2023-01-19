29048d606c6ca616ea62d55cf1ae2cb709cb07fe
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-19 09:28:57 +0300

    First

---
+++ Primer0-3.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+import random
+# Генерация случайного чисела для X и Y от 1 до 9
+x = random.randint(1,9)
+y = random.randint(1,9)
+
+# Вводим первое число
+#x = int(input("Введите число X: "))
+# Вводим второе число
+#y = int(input("Введите число Y: "))
+
+# Вычисляем значение
+a = x + y
+b = x - y
+c = x * y
+d = x / y
+e = x // y
+f = x ** y
+
+# Выводим на экран значение X и Y
+print("X =",x,"Y =",y)
+
+# Сравниваем X и Y между собой, результат выводим на экран
+if x < y:
+    print('X меньше Y')
+elif x == y:
+    print('X равен Y')
+else:
+    print('X больше Y')
+
+# Выводим результат примеров на экран
+print("X+Y =",a,"  X-Y =",b,"  X*Y =",c,"  X/Y =",d,"  X//Y =",e,"  X**Y =",f)
+
+# Сравнение результатов, код найден в интернете, применён для выполнения теукщей задачи
+
+# Сравниваем полученый результат через MAX
+l_num1 = (a, b, c, d, e, f)
+max_num = max(l_num1)
+print("Первое наибольшее число через MAX:", max_num)
+
+# Сравниваем полученый результат через FOR
+def calc_largest(arr):
+    second_largest = arr[0]
+    largest_val = arr[0]
+    for i in range(len(arr)):
+        if arr[i] > largest_val:
+            largest_val = arr[i]
+
+    for i in range(len(arr)):
+        if arr[i] > second_largest and arr[i] != largest_val:
+            second_largest = arr[i]
+
+    return second_largest
+
+z = calc_largest([a, b, c, d, e, f])
+print("Второе наибольшее число через FOR:",z)
