2e3c638d2fc7ec66628508d67f7f34dedd007a66
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-24 01:59:34 +0300

    Test_File

--- Task_3/Task_3-2.py
+++ Task_3/Task_3-2.py
@@ -1,2 +1,10 @@
 #! /usr/bin/python
 
+x = "12244355367386879098756"
+#x = int(input(Введите число :"))
+
+l_x = list(x)
+len_x = len(l_x)
+print(l_x)
+print(len_x)
+#for i in range(11):
--- Testing_Zone/Primer0-2.py
+++ Testing_Zone/Primer0-2.py
@@ -1,1 +1,8 @@
 #! /usr/bin/python
+
+entered_list = input("Введите список чисел, разделенных пробелом: ").split()
+print("Введенный список:", entered_list)
+
+num_list = list(map(int, entered_list))
+print("Список чисел: ", num_list)
+print("Сумма списка:", sum(num_list))
