226f2cb236b9be23a5615ae4ce61b3c20ced451d
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-23 21:25:19 +0300

    TestFile

--- Primer0-5.py
+++ Primer0-5.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python
 
+crl = (123, 234, 345, 456, 567, 678, 789, 890)
 n = int(input())
-lst = []
-for i in range(1, n+1):
-    for j in range(i):
-        lst.append(j)
-print(lst)
+for k,v in enumerate(crt):
+    if n => v and n <= crt[k+1]:
+        res = crt[:k+1]+(n,)+crt[k+1:]
+        break
+print(res)
