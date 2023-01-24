eeddf033975bd3aa0117460322f86fb2db0c43bb
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-25 00:59:39 +0300

    TestFile

--- Testing_Zone/Primer0-3.py
+++ Testing_Zone/Primer0-3.py
@@ -1,8 +1,18 @@
 #! /usr/bin/python
-b = 0
-while True:
-    a = int(input())
-    if a < 0:
-        break
-    b = b + a
-print(b)
+
+num = "01120044594502736723"
+l_i = i.split()
+print(l_i)
+print(len(i))
+s2 = sorted(l_i)
+print(s2)
+str1 = '13579'
+# initializing substring
+A = 1
+# create a result list
+result = []
+for i in range(0, len(str1), A):
+    # convert to int, after the slicing process
+    result.append(int(str1[i: i + A]))
+
+print("The resultant list : " + str(result))
