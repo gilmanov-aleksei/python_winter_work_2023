271fb923a533744b99f05848ee0082c92c2dac18
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-25 19:17:17 +0300

    Test_File

--- Testing_Zone/Primer0-1.py
+++ Testing_Zone/Primer0-1.py
@@ -1,1 +1,16 @@
 #! /usr/bin/python
+
+#person = {
+#    'name':'Mashs',
+#    'login':'kasha',
+#    'age': '25',
+#}
+#print(type(person))
+
+dct = {
+    None:'mango',
+    None:'coco'
+}
+dct[True] = 35
+dct['True'] = 47
+print(dct)
--- Testing_Zone/Primer0-2.py
+++ Testing_Zone/Primer0-2.py
@@ -1,5 +1,13 @@
 #! /usr/bin/python
 
-str_lst = ['1', '2', '3']
-int_lst = [int(x) for x in str_lst]
-print (int_lst)
+month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30,12:31}
+
+while True:
+    y, m = int(input()), int(input())
+    if y == 0 and m == 0:
+        break
+    if y % 4 == 0 and m == 2:
+        print('29')
+    else:
+        print(month[m])
+
