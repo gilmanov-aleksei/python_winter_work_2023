1cc1dead615b44076b20a215a25916422b2acce4
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-26 01:54:00 +0300

    Lesson4

--- Task_4/Task_4-1.py
+++ Task_4/Task_4-1.py
@@ -1,2 +1,19 @@
 #! /usr/bin/python
 
+abc = input().split()
+
+a = int(abc[0])
+b = int(abc[2])
+c = abc[1]
+if c == '+':
+    print(abc[0], '+', abc[2], '=', a + b)
+if c == '-':
+    print(abc[0], '-', abc[2], '=', a - b)
+if c == '*':
+    print(abc[0], '*', abc[2], '=', a * b)
+if c == '/':
+    if b != 0:
+        print(abc[0], '/', abc[2], '=', a / b)
+    else:
+        print("Деление на ноль!")
+
