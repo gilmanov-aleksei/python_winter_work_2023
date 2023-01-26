22dd400d3915a607b04e891d7388f1042e63f1c3
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-27 01:54:28 +0300

    TestFile

--- Testing_Zone/Primer0-6.py
+++ Testing_Zone/Primer0-6.py
@@ -1,10 +1,1 @@
 #! /usr/bin/python
-
-n = 5
-m = 6
-mas = {(x, y): 0 for x, y in range(n+1)}
-print(mas)
-
-lst = [[ i * j for j in range(m)] for i in range(n)]
-print(lst, end=' ')
-d = {a: a ** 2 for a in range(7)}
