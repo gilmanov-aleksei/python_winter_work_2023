c461cab7759566f07439b8b4e485b2c0d3837c6f
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-27 01:03:04 +0300

    Lesson4

--- Task_4/Task_4-0.py
+++ Task_4/Task_4-0.py
@@ -1,1 +1,21 @@
 #! /usr/bin/python
+
+s = input().split()
+
+word1 = list(s[0])
+word2 = list(s[1])
+lens1 = len(s[0])
+
+if lens1 != len(s[1]):
+    chek = False
+else:
+    word1.sort()
+    word2.sort()
+    chek = True
+    i = 0
+    while i < lens1:
+        if word1[i] == word2[i]:
+            i += 1
+        else:
+            chek = False
+print(chek)
