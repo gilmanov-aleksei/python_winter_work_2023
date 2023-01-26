ab45f9affa6d2143650c1f3f042ddb68f3235a0d
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-27 02:20:42 +0300

    Lesson4

--- Task_4/Task_4-0.py
+++ Task_4/Task_4-0.py
@@ -4,14 +4,14 @@ s = input('Введите два слова через пробел: ').split()
 
 word1 = list(s[0])
 word2 = list(s[1])
+chek = True
+i = 0
 
 if len(s[0]) != len(s[1]):
     chek = False
 else:
     word1.sort()
     word2.sort()
-    chek = True
-    i = 0
     while i < len(s[0]):
         if word1[i] == word2[i]:
             i += 1
