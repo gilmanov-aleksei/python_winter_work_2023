a090372f63479c3c2e8412412a19726079f0a2ea
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-27 19:08:55 +0300

    TestFile

--- Testing_Zone/Primer_1-0.py
+++ Testing_Zone/Primer_1-0.py
@@ -1,1 +1,16 @@
 #! /usr/bin/python
+
+n = int(input())
+sinon = {}
+
+for i in range(n):
+    s,z = input().split()
+    sinon[s] = z
+    sinon[z] = s
+print(sinon)
+while True:
+    x = input()
+    print(sinon.get(x, "нет синонима"))
+    if x == "stop":
+        break
+
