feab3f9c0d47cebd7c158add096b97de6ddd1d37
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-27 19:53:10 +0300

    TestFile

--- Testing_Zone/Primer_1-1.py
+++ Testing_Zone/Primer_1-1.py
@@ -1,4 +1,16 @@
 #! /usr/bin/python
 
-for k in [4, 3, 2, 1, 0]:
-    print(round(12.3456, k))
+# t = int(input())
+t = 3661
+s = t % 60
+m = t // 60
+m = m % 60
+h = t // 3600
+h = h % 3600
+d = t // 86400
+
+print(t , "=", h, m ,s)
+
+
+
+
