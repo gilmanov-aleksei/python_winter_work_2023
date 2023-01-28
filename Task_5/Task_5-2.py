7af82693cfd481089984c6bdef23c1cde0169a3c
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-29 01:50:35 +0300

    Lesson5

--- Task_5/Task_5-2.py
+++ Task_5/Task_5-2.py
@@ -1,2 +1,13 @@
 #! /usr/bin/python
 
+n = int(input("Введите число: "))
+d = {}
+k = 1
+for i in range(1, int(n / 2) + 1):
+    if n % i == 0:
+        d.setdefault(k, i)
+        k += 1
+d.setdefault(k, n)
+print(f"Для числа {str(n)} все его делители: ", end=' ')
+for k, v in d.items():
+    print(v, end=' ')
