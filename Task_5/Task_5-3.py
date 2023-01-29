d6b5423d067ea5641321da57c699f7b6b319d20e
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-29 03:38:11 +0300

    Lesson5

--- Task_5/Task_5-3.py
+++ Task_5/Task_5-3.py
@@ -1,24 +1,14 @@
 #! /usr/bin/python
 
-n = int(input("Номер элемента ряда Фибоначчи: "))
-f1 = 1
+n = int(input("Введите число: "))
+f1 = 0
 f2 = 1
 fs = 0
 i = 0
-while i < n - 2:
+print("Значение этого элемента:", f1, f2, end=' ')
+while i < n - 1:
     fs = f1 + f2
     f1 = f2
     f2 = fs
     i += 1
-
-print("Значение этого элемента:", f2)
-
-fib1 = fib2 = 1
-
-n = int(input())
-
-print(fib1, fib2, end=' ')
-
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end=' ')
+    print(f2, end=' ')
