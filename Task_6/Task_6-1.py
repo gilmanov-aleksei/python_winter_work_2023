dec9ea08ce36224dba393307d2536891ad0ec0f9
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-31 01:08:34 +0300

    Lesson6

--- Task_6/Task_6-1.py
+++ Task_6/Task_6-1.py
@@ -11,11 +11,14 @@ d_roma = { 'I': 1, 'IV': 4, 'V': 5, 'IX': 9,'X': 10,
             'XL': 40,'L': 50, 'XC': 90, 'C': 100,
            'CD': 400, 'D': 500, 'CM': 900, 'M': 1000
            }
+str_n = str_nums.split(sep=",")
+print(str_n)
 
 for i in str_nums:
 #    print(roma_nubers(i))
     print(i, end=" ")
 print()
+
 for k, v in d_roma.items():
     print(k, "-", v, end="  ")
 
