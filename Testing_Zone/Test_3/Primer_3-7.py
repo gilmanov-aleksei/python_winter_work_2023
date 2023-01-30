b77e0d4ef0fbd54f549371a4f78a11a8b5ac517e
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-30 21:34:29 +0300

    TestFile

--- Testing_Zone/Test_3/Primer_3-7.py
+++ Testing_Zone/Test_3/Primer_3-7.py
@@ -1,1 +1,13 @@
 
+def uni_let(lst):
+    tes = set()
+    for i in lst:
+        tes = tes.union(i)
+    print(tes)
+    sortes = sorted(tes)
+    string = ''.join(sortes)
+    return string, len(string)
+
+print(uni_let(['Hello', 'world']))
+
+
