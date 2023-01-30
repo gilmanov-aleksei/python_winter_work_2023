2c001226da2f0decb3946d544ad27561a879ef21
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-30 19:17:50 +0300

    Lesson5

---
+++ Testing_Zone/Test_3/Primer_3-1.py
@@ -0,0 +1,3 @@
+
+tes = set(map(int, input().split()))
+print(sum(tes) / len(tes)
---
+++ Testing_Zone/Test_3/Primer_3-2.py
@@ -0,0 +1,3 @@
+months = {"Jan", "Feb", "March", "Apr", "May", "June","July", "Aug", "Sep", "Okt", "Nov", "Dec"}
+for m in months:
+    print(m)
