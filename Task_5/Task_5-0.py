94842575d6d4048be172b82a988c9f1694e2aedd
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-30 15:06:37 +0300

    Lesson5

---
+++ Task_5/Task_5-0.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+#
+s = input("Enter a word: ")
+d = {'1': 'a', '2': 'e', '3': 'i', '4': 'o', '5': 'u'}
+
+s1 = sorted(list(s))
+gl = 0
+sg = 0
+print(s1)
+for i in range(len(s1) + 1):
+
+
+if gl > sg + 2 or gl :
+    if gl + 1 == sg:
+        pass
+    elif gl - 1 == sg:
+        pass
+
