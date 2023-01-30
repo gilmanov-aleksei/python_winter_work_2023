c0aae4b9f68ef43b28b09392328b2a3c34aa37f4
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-31 00:33:46 +0300

    Lesson6

--- Task_6/Task_6-3.py
+++ Task_6/Task_6-3.py
@@ -2,14 +2,17 @@
 
 # n = inprut("Enter a word: ")
 n = "ab18,c#2wc64-+*656fab=561xz!-baz35"
+# Выводим на печать множество только отсартированых букв
+print(* sorted(set(''.join(s for s in n if s.isalpha()))))
 
-str_0 = set(''.join(s for s in n if s.isalpha()))
-num_0 = set(''.join(s for s in n if s.isdigit()))
-znak1 = set(''.join(s for s in n if not s.isdigit()))
-znak2 = set(''.join(s for s in n if not s.isalpha()))
-print(*num_0)
-print(*str_0)
-print(*znak1 & znak2)
+# Выводим на печать множество только отсартированых цифр
+print(* sorted(set(''.join(s for s in n if s.isdigit()))))
+
+# Собираем из двух множеств, в одно нет цифр, вдругом нет букв,
+# Производим пересечение двух множеств.
+# Выводим на печать отсартированые символы.
+print(* sorted(set(''.join(s for s in n if not s.isdigit()))
+        & set(''.join(s for s in n if not s.isalpha()))))
 
 
 
