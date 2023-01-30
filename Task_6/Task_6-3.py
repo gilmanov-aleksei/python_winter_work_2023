76d0d0cdcb9bb14fc468ade3b8bf76962d3dd2f9
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-31 00:18:28 +0300

    Lesson6

--- Task_6/Task_6-3.py
+++ Task_6/Task_6-3.py
@@ -1,1 +1,15 @@
 #! /usr/bin/python
+
+# n = inprut("Enter a word: ")
+n = "ab18,c#2wc64-+*656fab=561xz!-baz35"
+
+str_0 = set(''.join(s for s in n if s.isalpha()))
+num_0 = set(''.join(s for s in n if s.isdigit()))
+znak1 = set(''.join(s for s in n if not s.isdigit()))
+znak2 = set(''.join(s for s in n if not s.isalpha()))
+print(*num_0)
+print(*str_0)
+print(*znak1 & znak2)
+
+
+
