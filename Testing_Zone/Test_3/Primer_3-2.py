921799425b8d6f4a788c24e4486d8606de9383b4
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-30 19:52:05 +0300

    TestFile

--- Testing_Zone/Test_3/Primer_3-2.py
+++ Testing_Zone/Test_3/Primer_3-2.py
@@ -1,3 +1,7 @@
-months = {"Jan", "Feb", "March", "Apr", "May", "June","July", "Aug", "Sep", "Okt", "Nov", "Dec"}
-for m in months:
-    print(m)
+
+months_a = set(["Jan", "Feb", "March", "Apr", "May", "June"])
+months_b = set(["May", "June", "July", "Aug", "Sep", "Okt", "Nov", "Dec"])
+all_mohths = months_a.union(months_b)
+print(all_mohths)
+
+
---
+++ Testing_Zone/Test_3/Primer_3-4.py
@@ -0,0 +1,11 @@
+
+set_a = {1, 2, 3, 4, 5}
+set_b = {4, 5, 6, 7, 8}
+
+symm_diff = set_a.symmetric_difference(set_b)
+print(symm_diff)
+
+print(set_a ^ set_b)
+# diff_set = set_a.difference(set_b)
+# print(diff_set)
+# print(set_a - set_b)
