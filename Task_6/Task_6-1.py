74ae93a0d04401cecfbe5286b1615a538e6e5ac1
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-31 01:04:18 +0300

    Lesson6

--- Task_6/Task_6-1.py
+++ Task_6/Task_6-1.py
@@ -1,1 +1,22 @@
 #! /usr/bin/python
+
+def roma_nubers(r_nums):
+
+    pass
+
+ #   return num_print
+
+str_nums = "MMXXIII, MMXXIV, MCMXVII, MXMLXI, MM, MDXXXLXII"
+d_roma = { 'I': 1, 'IV': 4, 'V': 5, 'IX': 9,'X': 10,
+            'XL': 40,'L': 50, 'XC': 90, 'C': 100,
+           'CD': 400, 'D': 500, 'CM': 900, 'M': 1000
+           }
+
+for i in str_nums:
+#    print(roma_nubers(i))
+    print(i, end=" ")
+print()
+for k, v in d_roma.items():
+    print(k, "-", v, end="  ")
+
+
