fcf8d18735c6bf7cf262d58172fb699d887743b6
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-01-30 18:42:56 +0300

    Lesson5

--- Task_5/Task_5-3.py
+++ Task_5/Task_5-3.py
@@ -2,8 +2,9 @@
 
 n = input("Введите число: ")
 f1 = f2 = 1
+n = int(n) - 2
 print("Значения элемента:", f1, f2, end=' ')
-while n - 2 > 0:
+while n > 0:
     f1, f2 = f2, f1 + f2
     n -= 1
     print(f2, end=' ')
