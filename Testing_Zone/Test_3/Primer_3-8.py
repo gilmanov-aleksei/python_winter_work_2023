9e1f4e06cff295fc75719c96801c63e60f763d9c
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-03 01:39:31 +0300

    TestFile

--- Testing_Zone/Test_3/Primer_3-8.py
+++ Testing_Zone/Test_3/Primer_3-8.py
@@ -1,6 +1,16 @@
 #! /usr/bin/python
 
-from math import pi, sqrt
-print(sqrt(121))
-print(pi)
-print(E)
+def result(num_my):
+
+    num = [54,24,34,1,2,3]
+    num_res = []
+
+    for number in num:
+        if number in num_my:
+            num_res.append(number)
+    return num_res
+num_1 = (result([54,24,1,2,3]))
+num_2 = (result([34,1,3]))
+
+print(num_1)
+print(num_2)
