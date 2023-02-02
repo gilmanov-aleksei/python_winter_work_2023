2d8eea911f869aa90045d8169d8c81588a2088d0
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-03 02:08:25 +0300

    TestFile

--- Testing_Zone/Test_3/Primer_3-9.py
+++ Testing_Zone/Test_3/Primer_3-9.py
@@ -1,1 +1,22 @@
 
+# создаем my_set
+my_set = {0}
+print(my_set)
+
+# вызов my_set[0] приведет к ошибке
+# TypeError: 'set' object does not support indexing
+
+# добавляем элемент
+# Вывод: {1, 2, 3}
+my_set.add(2)
+print(my_set)
+
+# добавляем несколько элементов
+# Вывод: {1, 2, 3, 4}
+my_set.update([2, 3, 4])
+print(my_set)
+
+# добавляем список и множество
+# Вывод: {1, 2, 3, 4, 5, 6, 8}
+my_set.update([4, 5], {1, 6, 8})
+print(my_set)
