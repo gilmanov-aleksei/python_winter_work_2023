2b7f8afe093392ac95ee78632ef70e492391fea1
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-03 01:25:31 +0300

    Lesson7

--- Task_7/Task_7-3.py
+++ Task_7/Task_7-3.py
@@ -1,3 +1,13 @@
 #! /usr/bin/python
 
-
+from random import randint
+x = []
+n = randint(2, 5)
+m = randint(2, 5)
+print(n, m)
+for i in range(n):
+    y = []
+    for j in range(m):
+        y.append(randint(10, 99))
+    x.append(y)
+print(x)
