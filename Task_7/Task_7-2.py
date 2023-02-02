ff54ed6593ea1bbb5b9608ba18475a51e4974e60
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-02 23:03:44 +0300

    Lesson7

--- Task_7/Task_7-2.py
+++ Task_7/Task_7-2.py
@@ -1,16 +1,25 @@
 #! /usr/bin/python
 
-# string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-string = "Hello - World!"
-str = ""
-n = 3
-for i in string:
-    if 65 <= ord(i) <=90:
-        str += chr(ord(i) + n)
-    elif 97 <= ord(i) <= 122:
-        str += chr(ord(i) + n)
-    else:
-        str += i
-print (str)
+def code(string, n):
+    chif = ""
+    for i in string:
+        if 65 <= ord(i) <=90:
+            if ord(i) + n > 90:
+                chif += chr((ord(i) + n - 64) % 26 + 65)
+            else:
+                chif += chr(ord(i) + n)
+        elif 97 <= ord(i) <= 122:
+            if ord(i) + n > 122:
+                chif += chr((ord(i) + n - 96) % 26 + 97)
+            else:
+                chif += chr(ord(i) + n)
+        else:
+            chif += i
+    return chif
 
+# str = input("Enter striing: ")
+str = "Zz - XHJVSJghb,dshgdjh. (hgwhgw) kjhGKJWSGWYgyx - 1212 b98d78c7yvvY - zxy!"
+# step = int(input("Enter step: "))
+step = 3
 
+print(code(str, step))
