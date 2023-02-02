d0795ad4173d1b13840f4b81e2247db5427f0749
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-02 20:46:54 +0300

    Lesson7

--- Task_7/Task_7-2.py
+++ Task_7/Task_7-2.py
@@ -1,2 +1,16 @@
 #! /usr/bin/python
 
+# string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+string = "Hello - World!"
+str = ""
+n = 3
+for i in string:
+    if 65 <= ord(i) <=90:
+        str += chr(ord(i) + n)
+    elif 97 <= ord(i) <= 122:
+        str += chr(ord(i) + n)
+    else:
+        str += i
+print (str)
+
+
