6456fb0a846705efd863ec279b221781997ff89d
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-02 23:24:48 +0300

    Lesson7

--- Task_7/Task_7-2.py
+++ Task_7/Task_7-2.py
@@ -3,23 +3,15 @@
 def code(string, n):
     chif = ""
     for i in string:
-        if 65 <= ord(i) <=90:
-            if ord(i) + n > 90:
-                chif += chr((ord(i) + n - 64) % 26 + 65)
-            else:
-                chif += chr(ord(i) + n)
+        if 65 <= ord(i) <= 90:
+            chif += chr((ord(i) + n - 65) % 26 + 65)
         elif 97 <= ord(i) <= 122:
-            if ord(i) + n > 122:
-                chif += chr((ord(i) + n - 96) % 26 + 97)
-            else:
-                chif += chr(ord(i) + n)
+            chif += chr((ord(i) + n - 97) % 26 + 97)
         else:
             chif += i
     return chif
 
-# str = input("Enter striing: ")
-str = "Zz - XHJVSJghb,dshgdjh. (hgwhgw) kjhGKJWSGWYgyx - 1212 b98d78c7yvvY - zxy!"
-# step = int(input("Enter step: "))
-step = 3
-
-print(code(str, step))
+txt = str(input("Enter string: "))
+step = int(input("Enter step: "))
+print(code(txt, step))
+print(txt)
