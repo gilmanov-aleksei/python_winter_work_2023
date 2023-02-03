c2951bf3a53558d10b4cd7eea7d6b3a8ee3036d2
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-03 19:39:51 +0300

    TestFile

--- Testing_Zone/Test_4/Primer_4-1.py
+++ Testing_Zone/Test_4/Primer_4-1.py
@@ -1,2 +1,14 @@
 #! /usr/bin/python
 
+s = 'АГЦТАГЦТАГЦТ'
+n = 3
+d = {}
+for i in range(len(s) - n + 1):
+    d[s[i:i + n]] = d.get(s[i:i + n], 0) + 1
+print(d)
+mx = max(d.values())
+for i in d:
+    if d[i] == mx:
+        print(i, mx)
+
+
