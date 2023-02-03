5ac4ce47eb02be3db7d9c3d1ced00b9a4c54bba3
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-03 20:58:40 +0300

    TestFile

--- Testing_Zone/Test_4/Primer_4-4.py
+++ Testing_Zone/Test_4/Primer_4-4.py
@@ -3,5 +3,9 @@
 #lst = [22, 11, 13, 14, 36, 1, 2]
 #print(sorted(lst, key = lambda x:(x % 2, x)))
 
-lst = ['b', 'A', 'Z', 'x']
-print(sorted(lst, key = lambda x: x.lower()))
+# lst = ['b', 'A', 'Z', 'x']
+# print(sorted(lst, key = lambda x: x.lower()))
+
+lst = [1, 10, 21, 30]
+x = 16
+print(min(lst, key=lambda y: abs(x - y)))
