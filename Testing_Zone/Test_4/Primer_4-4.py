07d306cf4b5c7161ed1057748ca3c145d6478cd4
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-03 21:20:43 +0300

    TestFile

--- Testing_Zone/Test_4/Primer_4-4.py
+++ Testing_Zone/Test_4/Primer_4-4.py
@@ -9,3 +9,7 @@
 lst = [1, 10, 21, 30]
 x = 16
 print(min(lst, key=lambda y: abs(x - y)))
+
+lst = [123, 234, 345, 456]
+sum_dig = lambda x: sum(map(int, str(x)))
+print(list(map(sum_dig, lst)))
--- Testing_Zone/Test_4/Primer_4-5.py
+++ Testing_Zone/Test_4/Primer_4-5.py
@@ -1,5 +1,2 @@
 #! /usr/bin/python
 
-lst = [123, 234, 345, 456]
-sum_dig = lambda x: sum(map(int, str(x)))
-print(list(map(sum_dig, lst)))
