aff9738a38fb3747c0a081dd377de89d99b5bcbb
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-05 01:47:35 +0300

    TestFile

--- Testing_Zone/Test_4/Primer_4-1.py
+++ Testing_Zone/Test_4/Primer_4-1.py
@@ -3,10 +3,14 @@
 # s = str(input("Введите генетический код: "))
 s = 'АГЦТААГГЦЦТТГАГАТЦТТАЦЦТАГТАГЦТ'
 r = [('АГ', 'ГА'), ('ЦТ', 'ЦАГТ')]
+# Вывод на экран исходной строки
 print(s)
 for i in r:
-    s = s.replace(i[0], i[1])
+    s = s.replace(*i)
 print(s)
 
-
-
+s = 'АГЦТААГГЦЦТТГАГАТЦТТАЦЦТАГТАГЦТ'
+r = [('АГ', 'ГА'), ('ЦТ', 'ЦАГТ')]
+mrep = lambda s, d: s if not d else mrep(s.replace(*d.pop()), d)
+print(type(mrep))
+print(mrep(s, r))
