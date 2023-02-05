4f02795839217eff2d88f3662832ce84db78cf05
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-06 01:05:25 +0300

    Lesson8

--- Task_8/Task_8-3.py
+++ Task_8/Task_8-3.py
@@ -1,3 +1,17 @@
 #! /usr/bin/python
 
+# Дан список слов. Отсортируйте его по количеству уникальных букв
+# в каждом слове в обратном порядке.
+# Например:  ['abab', 'xx', 'aaaaaaa', 'abcbab']
+# Результат:  ['abcbab', 'abab', 'aaaaaaa', 'xx']
+# Если число уникальных букв одинаково, то порядок алфавитный.
 
+lst = ['abab', 'xx', 'aaaaaaa', 'abcbab']
+elm = {}
+print(lst)
+for i in lst:
+    elm[i] = len(set(i))
+sort_t = sorted(elm.items(), key=lambda x: x[1], reverse=True)
+sort_d = {k: v for k, v in sort_t}
+
+print(sort_d)
