dee339dad75decc012cad60032a5029a04ecbcd2
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-06 19:25:39 +0300

    TestFile

--- Testing_Zone/Test_5/Test_5-1.py
+++ Testing_Zone/Test_5/Test_5-1.py
@@ -1,1 +1,11 @@
 #! /usr/bin/python
+
+students = {
+    1: {'name': 'Pete', 'age': 25, 'profession': 'developer'},
+    0: {'name': 'Nick', 'age': 26, 'profession': 'tester'}
+}
+
+for d in students:
+    print(d, students[d])
+    for i in students[d]:
+        print(i, students[d][i])
