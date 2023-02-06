4a3ca083916f3ba5b4740e7c6e22f1bb952420c7
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-06 21:07:51 +0300

    TestFile

--- Testing_Zone/Test_5/Test_5-6.py
+++ Testing_Zone/Test_5/Test_5-6.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 fr = open("test.txt", 'r', encoding="utf-8")
-fw = open("test1.txt", 'w', encoding="utf-8")
+fw = open("test1.txt", 'a', encoding="utf-8")
 
 lst = fr.readlines()
 
