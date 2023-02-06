56f11c1e7d1bc2b6905748ee4bb8a7836090f87e
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-06 21:46:12 +0300

    TestFile

--- Testing_Zone/Test_5/Test_5-5.py
+++ Testing_Zone/Test_5/Test_5-5.py
@@ -1,7 +1,17 @@
 #! /usr/bin/python
 
-f = open("text.txt", 'r', encoding='utf-8')
-lst = f.readlines()
-print(lst)
+fi = open("test.txt", 'r', encoding='utf-8')
+fo = open("test2.txt", 'w', encoding='utf-8')
 
-f.close()
+#lst = f.readlines()
+txt = fi.read()
+wxt = ''
+print(txt)
+for i, j in enumerate(txt):
+    if i % 2 == 0:
+        wxt +=j
+print(wxt)
+
+
+fo.close()
+fi.close()
