994bc5c7e85d7d9530281d5574740aa47019f476
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-08 16:29:37 +0300

    Lesson9

--- Task_9/Task_9-2.py
+++ Task_9/Task_9-2.py
@@ -16,8 +16,8 @@
 
 # str1 = input("Введите первое слов: ")
 # str2 = input("Введите слова через пробел: ")
-s1 = 'питон'
-s2 = 'поросенок титан итог лавка погост кино'
+s1 = 'питона'
+s2 = 'титан итог лавка погост кино' # поросенок
 glas = {'а': 0, 'у': 0, 'о': 0, 'ы': 0, 'и': 0, 'э': 0, 'я': 0, 'ю': 0, 'ё': 0, 'е': 0, }
 gls1 = 0
 sl1 = list(s1)
@@ -26,10 +26,13 @@ for i, j in enumerate(sl1):
         gls1 += 1
 print('Слова, похожие на:', s1)
 sl2 = s2.split()
+lsl2 = len(sl2)
 for i in sl2:
     sl3 = list(i)
+    lsl3 = len(i)
     gls2 = 0
-    if len(sl3) + 1 <= len(sl2) or len(sl2) == len(sl3) - 1 or len(sl2) == len(sl3):
+    print(i)
+    if lsl2 == lsl3 + 1 or lsl2 == lsl3:
         for c, v in enumerate(sl1):
             if v in glas:
                 if sl3[c] in glas:
