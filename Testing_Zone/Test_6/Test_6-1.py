49bdfed6b74caf2c3cd551cd1432de75779ca442
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-08 19:03:48 +0300

    TestFile

--- Testing_Zone/Test_6/Test_6-1.py
+++ Testing_Zone/Test_6/Test_6-1.py
@@ -1,1 +1,11 @@
 #! /usr/bin/python
+
+dct = {1: {11: {111:1111}}, 2: {22: {222:2222}}, 3: {33:333}, 4: 444}
+for i in dct:
+    print(i, dct[i])
+    if type(dct[i]) == dict:
+        for j in dct[i]:
+            print(j, dct[i][j])
+            if type(dct[i][j]) == dict:
+                for k in dct[i][j]:
+                    print(k, dct[i][j][k])
