1eb71c7a98df1468e9a80ce9790b3718aba4cde8
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-09 00:09:30 +0300

    Lesson10

--- Task_10/Task_10-0.py
+++ Task_10/Task_10-0.py
@@ -1,2 +1,15 @@
 #! /usr/bin/python
 
+import openpyxl
+# from openpyxl import Workbook
+s = []
+wb = openpyxl.load_workbook('task_10-0.xlsx')
+# print(wb.sheetnames)
+for i in wb.sheetnames:
+    ws = wb[i]
+    s.append((ws.title, ws.max_row * ws.max_column))
+print(s)
+print(sorted(s, key=lambda x: (x[0], x[1])))
+print(sorted(s, key=lambda x: (-x[1], x[0])))
+
+
