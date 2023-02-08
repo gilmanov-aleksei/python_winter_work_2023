203c60496a89c5e1cd068e096569a283eafa5b2d
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-09 01:13:56 +0300

    Lesson10

--- Task_10/Task_10-1.py
+++ Task_10/Task_10-1.py
@@ -4,5 +4,21 @@ import openpyxl
 mr = 0
 mc = 0
 fio = ''
+d ={}
+
 wb = openpyxl.load_workbook("task_10-1.xlsx")
-print(wb.sheetnames)
+str = wb.sheetnames
+print(str)
+ws = wb[str[0]]
+print(ws.title)
+for i in range(ws.max_row):
+    zp = []
+    for j in range(ws.max_column):
+        if j != 0:
+            zp.append(ws.cell(row=i + 1, column=j + 1).value)
+        else:
+            fio = ws.cell(row=i + 1, column=j + 1).value
+    d.setdefault(fio, zp)
+print(d)
+ws = wb[str[1]]
+print(ws.title)
