41cf521ce8c496b986eaac68e321a23521b6e06f
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-08 19:59:56 +0300

    TestFile

--- Testing_Zone/Test_6/Test_6-3.py
+++ Testing_Zone/Test_6/Test_6-3.py
@@ -2,11 +2,22 @@
 
 import csv
 
-with open('products.csv', encoding='utf-8') as file:
-    rows = csv.reader(file)
-    for row in rows:
-        print(row)
+# with open('test.csv', encoding='utf-8') as file:
+#     rows = csv.reader(file)
+#     for row in rows:
+#         print(row)
 
+columns = ['first_name', 'second_name', 'riting']
+data = [['1', 'Ivanov', '200'],
+        ['2', 'Petrov', '300'],
+        ['3', 'Sidorov', '400'],
+        ['4', 'Makarov', '500']]
+
+with open('test1.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(columns)
+    for row in data:
+        writer.writerow(row)
 
     # data = fi.read()
     # for line in data.splitlines():
