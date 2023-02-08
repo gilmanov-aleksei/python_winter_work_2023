1cf11f3ccfc3b9d3647f1d4a7323a8b84e8bd45d
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-08 20:49:02 +0300

    TestFile

--- Testing_Zone/Test_6/Test_6-5.py
+++ Testing_Zone/Test_6/Test_6-5.py
@@ -2,5 +2,13 @@
 
 import openpyxl
 # from openpyxl import Workbook
-wb = openpyxl.Workbook()
+wb = openpyxl.load_workbook("test.xlsx")
 wb.save('test.xlsx')
+ws = wb.active
+wb.active = ws
+print(ws.title)
+print(wb.sheetnames)
+wb.create_sheet("Newssheet")
+
+
+#
