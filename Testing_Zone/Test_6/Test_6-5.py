3fe4bc62569653a63c0a066c304d7fe58769d562
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-08 21:21:42 +0300

    TestFile

--- Testing_Zone/Test_6/Test_6-5.py
+++ Testing_Zone/Test_6/Test_6-5.py
@@ -3,12 +3,26 @@
 import openpyxl
 # from openpyxl import Workbook
 wb = openpyxl.load_workbook("test.xlsx")
-wb.save('test.xlsx')
-ws = wb.active
-wb.active = ws
-print(ws.title)
 print(wb.sheetnames)
-wb.create_sheet("Newssheet")
+ws = wb.active
+print(ws)
+print(ws.title)
+wb.create_sheet("Sheet1")
+sheet = wb['Sheet1']
+wb.active = sheet
+ws['A4'].value = 1234
+c = ws['A4']
+print(c.coordinate, c.value)
+c.value = c.value * 2
+print(ws.max_row)
+for i in range(ws.max_row):
+    for j in range(ws.max_column):
+        print(i + 1, j + 1, ws.coll(row = i + 1, column = j + 1).value)
 
-
-#
+# wb.save('test.xlsx')
+# ws = wb.active
+# print(ws)
+# wb.active = ws
+# print(ws.title)
+# print(wb.sheetnames)
+# wb.create_sheet("Newssheet")
