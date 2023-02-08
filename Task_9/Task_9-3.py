646c998ef348cb081f7c5605834c953692b5a595
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-08 03:18:21 +0300

    Lesson8

--- Task_9/Task_9-3.py
+++ Task_9/Task_9-3.py
@@ -1,2 +1,45 @@
 #! /usr/bin/python
 
+# Дан генетический код ДНК (строка, состоящая из букв G, C, T, A)
+# Постройте РНК, используя принцип замены букв:
+# G>C; C>G; T>A; A>T
+# Напишите функцию, которая на вход получает ДНК, и возвращает РНК, для этого постройте словарь для замены букв.
+# Например:
+# Вход:GGCTAA Результат: CCGATT
+
+
+def dna_to_rnk(dna):
+    #
+    rnk = ""
+    #
+    for k in dna:
+        #
+        rnk += ddna.get(k)
+    #
+    return rnk
+#
+ddna = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'T'
+}
+#
+dnk = ""
+#
+sdnk = str(input("Enter DNA letters: ")).upper()
+#
+keys = ddna.keys()
+#
+for i in sdnk:
+    #
+    if i in keys:
+        #
+        dnk += ddna.get(i)
+# dnk = 'GGCTAA'
+#
+print(dnk)
+#
+print("-" * len(dnk))
+#
+print(dna_to_rnk(dnk))
