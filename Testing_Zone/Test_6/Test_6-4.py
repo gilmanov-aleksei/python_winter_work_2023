11d4c402acb8f55a715aa45e091b387291caba1f
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-08 19:32:22 +0300

    TestFile

--- Testing_Zone/Test_6/Test_6-4.py
+++ Testing_Zone/Test_6/Test_6-4.py
@@ -1,7 +1,11 @@
 #! /usr/bin/python
 
 lst = []
-with open('test.txt', 'r', encoding='utf-8') as fi:
+with open('test.txt') as fi:
     for i in fi:
         lst.append(i.strip())
     print(lst)
+    with open('test.txt', 'w') as fo:
+        for line in lst:
+            res = sorted(line.split())
+            fo.write(' '.join(res) + '\n')
