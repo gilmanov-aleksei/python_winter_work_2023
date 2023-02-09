84c1caf86588604ca38ee7544c29a1f79e10e39c
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-09 17:25:53 +0300

    Lesson10

--- Task_10/TAsk_10-2.py
+++ Task_10/TAsk_10-2.py
@@ -1,2 +1,13 @@
 #! /usr/bin/python
 
+# Импортируем модуль openpyxl
+import openpyxl
+from russian_names import RussianNames
+
+rn = RussianNames(count=10, patronymic=False, name=False, surname=True)
+fio = rn.get_batch()
+print(fio)
+
+wb = openpyxl.load_workbook("task_10-2.xlsx")
+sheet = wb.sheetnames
+ws = wb[sheet[0]]
