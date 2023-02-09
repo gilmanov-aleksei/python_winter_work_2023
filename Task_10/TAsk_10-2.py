98da4e8e52d9d084a7430341f2b216aac200ca73
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-09 22:37:28 +0300

    Lesson10

--- Task_10/TAsk_10-2.py
+++ Task_10/TAsk_10-2.py
@@ -2,11 +2,8 @@
 
 # Импортируем модуль openpyxl
 import openpyxl
-
-# gen_fio_sheet(1, 15)
-
-
 wb = openpyxl.load_workbook("task_10-2.xlsx")
 sheets = wb.sheetnames
+
 # ws = wb[sheets[s]]
 # wb.active = sheet
