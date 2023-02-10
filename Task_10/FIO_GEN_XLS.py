59fad1089be2059303ac766f92bfd1d0d6fc8d6a
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-10 10:26:43 +0300

    Lesson10

--- Task_10/FIO_GEN_XLS.py
+++ Task_10/FIO_GEN_XLS.py
@@ -7,6 +7,7 @@ from russian_names import RussianNames
 
 def gen_fio_sheet(s, fio, bl, el, name_file):
     # Генератор русских фамилий, имён и отчества русскими буквами
+    global b, e
     rn = RussianNames(count=fio, patronymic=False, name=False, surname=True)
     wb = openpyxl.load_workbook(name_file)
     sheets = wb.sheetnames
@@ -15,7 +16,7 @@ def gen_fio_sheet(s, fio, bl, el, name_file):
     for v, k in enumerate(fio, 1):
         ws['A' + str(v)] = k
         b = ord(bl)
-        e = ord(el + 1)
+        e = ord(el) + 1
     for i in range(b, e):
         for j in range(1, ws.max_row + 1):
             ws[str(chr(i)) + str(j)] = int(randint(0, 9))
