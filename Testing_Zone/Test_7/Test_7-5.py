d98ceec1d5cb9358b5e37e501d86f0a1e2159e07
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-10 23:13:15 +0300

    Lesson11

--- Testing_Zone/Test_7/Test_7-5.py
+++ Testing_Zone/Test_7/Test_7-5.py
@@ -1,2 +1,13 @@
 #! /usr/bin/python
 
+import calendar
+
+year = int(input('Введите номер года: '))
+dw = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+
+for month in range(1, 13):
+   for day in range(1, calendar.monthrange(year, month)[1] + 1):
+       dd = calendar.weekday(year, month, day)
+       # dw[dd] += 1
+       dw[dd] = dw.get(dd, 0) + 1
+print(dw)
