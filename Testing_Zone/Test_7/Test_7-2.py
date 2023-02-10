b67d6cc026ef673f765da8fe4cd725290d43fc32
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-10 20:27:39 +0300

    TestFile

--- Testing_Zone/Test_7/Test_7-2.py
+++ Testing_Zone/Test_7/Test_7-2.py
@@ -1,2 +1,23 @@
 #! /usr/bin/python
 
+import time
+
+t0 = time.time()
+# t = (2023, 2, 10, 19, 58, 0, 4 ,0 ,0)
+# print(time.asctime(t))
+print(time.ctime(t0))
+time.sleep(6)
+t1 = time.time()
+print(time.ctime(t1))
+print(f'Длительность: {round(t1 - t0)} сек.')
+
+# x = 1000000
+# for i in range(0, x * 10, x):
+#     t0 = time.time()
+#     su = 0
+#     for j in range(i):
+#         su += 1
+#     t1 = time.time()
+#     print(i, t1 - t0)
+
+
--- Testing_Zone/Test_7/Test_7-3.py
+++ Testing_Zone/Test_7/Test_7-3.py
@@ -1,2 +1,31 @@
 #! /usr/bin/python
 
+import time
+
+
+# def two_sleep():
+#     t0 = time.time()
+#     time.sleep(2)
+#     t1 = time.time()
+#     return t1 - t0
+
+
+# def three_sleep(s):
+def sleeping(s):
+    t0 = time.time()
+    time.sleep(s)
+    t1 = time.time()
+    return t1 - t0
+
+two = 0
+three = 0
+
+for i in range(11):
+    if i % 2 == 0:
+        # print(i, '- two')
+        two += sleeping(2)
+    else:
+        # print(i, '- three')
+        three += sleeping(3)
+print('two sleep: ', two)
+print('three sleep: ', three)
