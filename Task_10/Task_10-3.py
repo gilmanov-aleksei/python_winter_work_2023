62e11f90c0a1d96b8a86d544e71cce553b4a5b83
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-10 04:18:18 +0300

    Lesson10

--- Task_10/Task_10-3.py
+++ Task_10/Task_10-3.py
@@ -3,6 +3,7 @@
 
 # Импортируем модуль openpyxl
 import openpyxl
+import statistics
 
 # Открываем на чтение рабочую книгу
 wb = openpyxl.load_workbook("task_10-3.xlsx")
@@ -11,7 +12,7 @@ sheet = wb.sheetnames
 # Записываем в переменную первую страницу
 ws = wb[sheet[0]]
 # пустой словарь
-d = {}
+dct = {}
 # Цикл по максимальной строке на странице
 for i in range(ws.max_row):
     # Пустой список для записи двух ячеек
@@ -19,38 +20,44 @@ for i in range(ws.max_row):
     # Цикл по максимальной колонке на странице
     for j in range(ws.max_column):
         # Записываем две ячейки из строку в список
-        z.append(ws.cell(row=i + 1, column=j + 1).value)
-    # Проверяем значения списка со словарём
-    if d.get(z[0]):
-        # если ключ есть, то прибавляем значение
-        d[z[0]] += z[1]
-    else:
-        # если ключа нет, то создаём новый
-        # со значением из списка
-        d[z[0]] = z[1]
-
+        x = ws.cell(row=i + 1, column=j + 1).value
+        # Прверяем значение в ячейке, если есть, то
+        if x is not None:
+            # записываем в список, если нет, пропускаем
+            z.append(x)
+    # Берем из списка имя и удаляем его из списка
+    y = z.pop(0)
+    # имя это ключ в словаре, список цифр в значение
+    dct[y] = z
+for k, v in dct.items():
+    m = [min(v), max(v), round(statistics.mean(v), 1), statistics.median(v)]
+    dct[k] = m
+dct = dict(sorted(dct.items()))
+print(dct)
 
 # Открываем на чтение рабочую книгу
-wb1 = openpyxl.load_workbook("task_10-2.xlsx")
+wb = openpyxl.load_workbook("task_10-2.xlsx")
 # Прверяем в книги, есть ли Лист3,
-if 'Лист3' in wb1.sheetnames:
+if 'Лист2' in wb.sheetnames:
     # если есть удаляем его
-    wb1.remove(wb1["Лист3"])
+    wb.remove(wb["Лист2"])
 # Создаём третию страницу в рабочей книге
-wb1.create_sheet("Лист3")
+wb.create_sheet("Лист2")
 # Записываем в переменную все страницы рабочей книги
-sheets = wb1.sheetnames
+sheets = wb.sheetnames
 # Переключаемся на третию страницу
-ws1 = wb1[sheets[2]]
+ws = wb[sheets[1]]
 # Счетчик строк
-row = 1
-# Цикл по словарю
-for k, v in ds0.items():
-    # Записваем фамилии в первый столбик
-    ws1['A' + str(row)] = k
-    # Записываем значение во второй столбик
-    ws1['B' + str(row)] = v
-    # Увеличиваем счетчик строк
+
+for k, v in dct.items():
+    row = 1
+    for i, j in enumerate(range(65, 70), 1):
+        # ws[str(chr(i)) + str(row)] = k
+        print(str(chr(i)) + str(j), "=", k)
+
+        print(str(chr(i)) + str(j), "=", v[j - 1])
+        # ws[str(chr(i)) + str(j)] = v[j - 1]
     row += 1
+
 # Сохраняем все вычисления в файл
-wb1.save("task_10-3.xlsx")
+wb.save("task_10-3.xlsx")
