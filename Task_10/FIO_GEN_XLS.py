104341aac29fb87551223b09ae65f76bae0777ce
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-10 10:19:21 +0300

    Lesson10

--- Task_10/FIO_GEN_XLS.py
+++ Task_10/FIO_GEN_XLS.py
@@ -5,7 +5,7 @@ from random import randint
 from russian_names import RussianNames
 
 
-def gen_fio_sheet(s, fio, name_file):
+def gen_fio_sheet(s, fio, bl, el, name_file):
     # Генератор русских фамилий, имён и отчества русскими буквами
     rn = RussianNames(count=fio, patronymic=False, name=False, surname=True)
     wb = openpyxl.load_workbook(name_file)
@@ -14,15 +14,20 @@ def gen_fio_sheet(s, fio, name_file):
     fio = rn.get_batch()
     for v, k in enumerate(fio, 1):
         ws['A' + str(v)] = k
-    for i in range(66, 76):
+        b = ord(bl)
+        e = ord(el + 1)
+    for i in range(b, e):
         for j in range(1, ws.max_row + 1):
             ws[str(chr(i)) + str(j)] = int(randint(0, 9))
     wb.save(name_file)
     return
 
 
-# Генератор фамилий и 5 колонок значений,
-# первый аргумент - это номер листа,
-# второй аргумент - это колличество фамилий
-# третий аргумент - это имя файла, для сохранения результата
-gen_fio_sheet(0, 15, "task_10-3.xlsx")
+# Генератор фамилий из N колонок значений,
+# первый аргумент - номер листа куда будут
+# записываться фамилиии данные,
+# второй аргумент - колличество фамилий в колонке А
+# третий аргумент - с какой колонки начать вносить цифры
+# четвертый аргумент - какой колонкой закончить внесение цифр
+# пятый аргумент - это имя файла, для сохранения результата
+gen_fio_sheet(0, 15, 'B', 'K', 'task_10-3.xlsx')
