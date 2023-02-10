d8eea7d30c8349e8d060e3d2aaf1d5433a71d34e
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-10 19:08:55 +0300

    TestFile

--- Testing_Zone/Test_7/Test_7-1.py
+++ Testing_Zone/Test_7/Test_7-1.py
@@ -1,1 +1,11 @@
 #! /usr/bin/python
+
+import openpyxl
+
+wb = openpyxl.load_workbook("test_7.xlsx")
+for sh in wb.sheetnames:
+    ws = wb[sh]
+    print(ws.title)
+    for i in range(ws.max_row):
+        for j in range(ws.max_column):
+            print(i + 1, j + 1, ws.cell(i + 1, j + 1).value)
