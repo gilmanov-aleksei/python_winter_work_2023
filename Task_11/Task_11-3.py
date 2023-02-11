c5ea1eedc1803234e78b62730e4b5f4bdd4b8d6e
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-11 11:16:06 +0300

    Lesson11

--- Task_11/Task_11-3.py
+++ Task_11/Task_11-3.py
@@ -1,2 +1,23 @@
 #! /usr/bin/python
 
+def enter_num():
+    print('Перевод цифр, арабские в римские')
+    while True:
+        a = input('Введите число, 0 - выход: ')
+        if a == '0':
+            break
+        elif not a.isdigit():
+            print("Вы ввели не число, повторите ввод")
+        elif len(a) > 4:
+            print("Вы ввели большое число, повторите ввод")
+        else:
+            print(int(a))
+            return a
+
+
+d = {}
+n = enter_num()
+if n is not None:
+    print(n)
+else:
+    print("Exit")
