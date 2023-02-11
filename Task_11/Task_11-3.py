7c337e045456104a5e1c2132735c14958abfc7e7
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-11 12:36:34 +0300

    Lesson11

--- Task_11/Task_11-3.py
+++ Task_11/Task_11-3.py
@@ -14,9 +14,55 @@ def enter_num():
             return a
 
 
+def checkio1(data):
+    base = "I" * data
+
+    base = base.replace("I" * 5, "V")
+    base = base.replace("V" * 2, "X")
+    base = base.replace("X" * 5, "L")
+    base = base.replace("L" * 2, "C")
+    base = base.replace("C" * 5, "D")
+    base = base.replace("D" * 2, "M")
+
+    base = base.replace("DCCCC", "CM")
+    base = base.replace("CCCC", "CD")
+    base = base.replace("LXXXX", "XC")
+    base = base.replace("XXXX", "XL")
+    base = base.replace("VIIII", "IX")
+    base = base.replace("IIII", "IV")
+
+    return base
+
+
+def checkio2(data):
+    ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    hunds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    thous = ["", "M", "MM", "MMM", "MMMM"]
+
+    t = thous[data // 1000]
+    h = hunds[data // 100 % 10]
+    te = tens[data // 10 % 10]
+    o = ones[data % 10]
+
+    return t + h + te + o
+
+
+def checkio3(n):
+    result = ''
+    for arabic, roman in zip((1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
+                             'M     CM   D    CD   C    XC  L   XL  X   IX V  IV I'.split()):
+        result += n // arabic * roman
+        n %= arabic
+        # print('({}) {} => {}'.format(roman, n, result))
+    return result
+
+
 d = {}
-n = enter_num()
-if n is not None:
-    print(n)
-else:
+num = enter_num()
+if num is None:
     print("Exit")
+else:
+    print(checkio1(int(num)))
+    print(checkio2(int(num)))
+    print(checkio3(int(num)))
