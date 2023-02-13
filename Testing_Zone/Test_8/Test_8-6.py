e5fc049a5a975291cc6527efe23f63eefc38cb94
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-13 20:10:35 +0300

    TestFile

--- Testing_Zone/Test_8/Test_8-6.py
+++ Testing_Zone/Test_8/Test_8-6.py
@@ -1,1 +1,8 @@
 #! /usr/bin/python
+
+original_prices = [1.25, -9.45, 10.22, 3.78, -5.92, 1.16]
+def get_price(price):
+    return price if price > 0 else 0
+
+prices = [get_price(i) for i in original_prices]
+print(prices)
