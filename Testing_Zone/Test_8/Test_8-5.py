037730679fda5f6c10605f9e791a4db65a6bc37e
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-13 19:35:52 +0300

    TestFile

--- Testing_Zone/Test_8/Test_8-5.py
+++ Testing_Zone/Test_8/Test_8-5.py
@@ -1,1 +1,17 @@
 #! /usr/bin/python
+
+import time
+
+y = 100000000
+t0 = time.time()
+s = []
+for i in range(y):
+    s.append(i ** 3)
+t1 = time.time()
+a = list(map(lambda x: x ** 3, range(y)))
+t2 = time.time()
+b = [x ** 3 for x in range(y)]
+t3 = time.time()
+print(t1 - t0)
+print(t2 - t1)
+print(t3 - t2)
--- Testing_Zone/Test_8/Test_8-7.py
+++ Testing_Zone/Test_8/Test_8-7.py
@@ -1,1 +1,3 @@
 #! /usr/bin/python
+
+a = [x for x in range]
