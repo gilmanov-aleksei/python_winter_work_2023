14c8b4bd69502c770966f3c9eb1f97248ed7a2c1
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-13 11:48:39 +0300

    TestFile

--- Testing_Zone/Test_7/Test_7-5.py
+++ Testing_Zone/Test_7/Test_7-5.py
@@ -1,14 +1,42 @@
 #! /usr/bin/python
 
-
+import datetime
+from datetime import datetime
+import datetime
 import calendar
+import locale
+from calendar import Calendar
+
+locale.setlocale(locale.LC_ALL, 'ru')
+
+today = datetime.datetime.now()
+print(datetime.datetime.strftime(today, '%d %B'))
 
 year = int(input('Введите номер года: '))
-dw = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+
+obj1 = calendar.Calendar(firstweekday = 3)
+
+for day in obj1.itermonthdates(year, 1):
+    print(day, end=' ')
+print()
+
+obj2 = calendar.Calendar()
+for day in obj2.itermonthdates(year, 9):
+    print(day, end=' ')
+print()
 
 for month in range(1, 13):
-   for day in range(1, calendar.monthrange(year, month)[1] + 1):
-       dd = calendar.weekday(year, month, day)
-       # dw[dd] += 1
-       dw[dd] = dw.get(dd, 0) + 1
-print(dw)
+    print(month)
+    for day in range(1, calendar.monthrange(year, month)[1] + 1):
+        print(calendar.weekday(year, month, day), end=' ')
+    input()
+    print()
+       # dw[dd] = dw.get(dd, 0) + 1
+# print(dw)
+
+
+# now = datetime.now()  # текущие дата и время
+# print(now)
+# # datetime.datetime(2021, 2, 23, 14, 46, 19, 60107)
+# print(datetime.weekday(now))  # день недели в виде числа, понедельник - 0, воскресенье - 6
+# print(datetime.isoweekday(now))  # день недели в виде числа, понедельник - 1, воскресенье - 7
--- Testing_Zone/Test_7/Test_7-6.py
+++ Testing_Zone/Test_7/Test_7-6.py
@@ -1,1 +1,10 @@
 #! /usr/bin/python
+
+#import datetime package
+import datetime
+import locale
+
+locale.setlocale(locale.LC_ALL, 'ru')
+
+today = datetime.datetime.now()
+print(datetime.datetime.strftime(today, '%d %B'))
