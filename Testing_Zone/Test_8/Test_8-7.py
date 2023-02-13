466e7f7dbf323bd55368225eb79f32811e5370b3
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-13 21:05:39 +0300

    TestFile

---
+++ Testing_Zone/Test_8/Test_8-7.py
@@ -0,0 +1,36 @@
+# #! /usr/bin/python
+#
+# def fu(x):
+#     return 'FizzBuzz' if x % 15 == 0 else 'Fizz' if x % 3 == 0 else 'Buzz' if x % 5 == 0 else x
+#
+#
+# a = [fu(x) for x in range(20)]
+# print(a)
+#
+# words = ['Я', 'изучаю', 'Python']
+# res = [letter for word in words for letter in word]
+# print(res)
+#
+# matrix = [[i ** j for i in range(5)] for j in range(6)]
+# print(matrix)
+#
+# matrix = [
+#     [0, 0, 0],
+#     [1, 1, 1],
+#     [2, 2, 2],
+# ]
+# flat = [num for row in matrix for num in row]
+# print(flat)
+#
+# t = [[x * y for x in range(1, 6)] for y in range(1, 6)]
+# print(t)
+#
+# d = {}
+# for num in range(1, 10):
+#     d[num] = num ** 2
+# print(d)
+
+# d={num: num **2 for num in range(1.10)}
+# rus_let = 'воититивоиытл12543265@#@%$^%&^'
+# dct = {i:ord(i) for i in rus_let}
+# print(dct)
