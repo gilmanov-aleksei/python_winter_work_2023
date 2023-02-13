aec4fc3f8354ec8d8cd4385ee10fe20c8da07528
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-13 18:57:53 +0300

    TestFile

--- Testing_Zone/Test_8/Test_8-2.py
+++ Testing_Zone/Test_8/Test_8-2.py
@@ -1,1 +1,11 @@
 #! /usr/bin/python
+
+import datetime, locale
+
+locale.setlocale(locale.LC_ALL, 'ru')
+a = datetime.datetime.today() + datetime.timedelta(days=14)
+print(a)
+c = datetime.timedelta(days=1)
+for _ in range(10):
+    a += c
+    print(a.strftime('%A %d, %B %Y'), "-", a)
