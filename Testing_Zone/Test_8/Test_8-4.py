08762066c0c6b329745569d1afa1afb19da4dc0b
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-13 20:06:50 +0300

    TestFile

--- Testing_Zone/Test_8/Test_8-4.py
+++ Testing_Zone/Test_8/Test_8-4.py
@@ -1,3 +1,8 @@
 #! /usr/bin/python
 
 
+from string import ascii_letters
+vowel = 'aeiouаеёиоуэюа'
+letters = 'gahjрouрро12аио676'
+print([x+'-Yes' if x in vowel else x+'-No' for x in letters])
+# is_eng = [f'{ltr}-ДА' if ltr in ascii_letters else f'{ltr}-НЕТ' for ltr in letters]
