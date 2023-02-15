4890c2057c93470bba8bf731034f6f6c06b28d2f
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-15 20:13:04 +0300

    TestFile

--- Testing_Zone/Test_9/Test_9-3.py
+++ Testing_Zone/Test_9/Test_9-3.py
@@ -1,3 +1,21 @@
 #! /usr/bin/python
+#
+# def fun(n):
+#     for x in range(n):
+#         yield x * x
+# g = fun(3)
+# for k in g:
+#     print(k)
+#     print(type(g))
+def fun(n):
+    for x in range(n):
+        print("До yield", x)
+        yield x * x
+        print("После yield")
+        print(x ** 3)
+g = fun(5)
 
-int[1,2]
+for k in g:
+    print("Перед печатью x * x")
+    print(f"k = {k}")
+    print("После печати x * x")
