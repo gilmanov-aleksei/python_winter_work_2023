55ecb35a6ddca0451bf6be198783b2fb1586ad7d
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-15 19:04:26 +0300

    Lesson5

--- Testing_Zone/Test_9/Test_9-1.py
+++ Testing_Zone/Test_9/Test_9-1.py
@@ -1,1 +1,11 @@
 #! /usr/bin/python
+
+d = []
+for i in range(1, 11):
+    if i % 2 == 0:
+        d.append(i * i)
+    else:
+        d.append(i ** 3)
+print(d)
+
+print([i ** 3 if i % 2 else i * i for i in range(1, 11)])
