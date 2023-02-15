270939695e77d2fa2fc80b112ffbc63b6cbc11c3
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-16 02:07:13 +0300

    Lesson 13

--- Task_13/Task_13-2.py
+++ Task_13/Task_13-2.py
@@ -8,21 +8,13 @@
 # 101 111 121 131 141 151 161 171 181 191 202 212 ...
 
 # def fu():
-# for i in range(1, 200):
-#     a = str(i)
-#     b = ''
-#     for j in range(len(a) - 1, -1, -1):
-#         b += a[j]
-#     if a == b:
-#         print(i, end=" ")
-
-import math
-def is_palindrom(purpose):
-    for i in range(1,int(math.sqrt(purpose))):
-        if purpose % i == 0:
-            print(i, purpose//i)
-is_palindrom(100)
-
+for i in range(1, 200):
+    a = str(i)
+    b = ''
+    for j in range(len(a) - 1, -1, -1):
+        b += a[j]
+    if a == b:
+        print(i, end=" ")
 
 # gf = fu()
 # for i in gf:
