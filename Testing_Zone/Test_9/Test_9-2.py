43df8999500ef05c3010444887f2ffc7960e9a58
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-15 19:35:57 +0300

    TestFile

--- Testing_Zone/Test_9/Test_9-2.py
+++ Testing_Zone/Test_9/Test_9-2.py
@@ -1,1 +1,16 @@
 #! /usr/bin/python
+
+while True:
+    try:
+        s = int(input("Введите целое число, 0 - Выход: "))
+    except:
+        print("Вы вели не целое число")
+        print("Повторите ввод!")
+    else:
+        if s == 0:
+            break
+        else:
+            print(s)
+            print("Повторите ввод!")
+    finally:
+        print()
