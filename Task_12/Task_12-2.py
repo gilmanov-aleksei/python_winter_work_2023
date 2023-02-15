28a2950c6ce92954e5a9073ca5a86bf843dd1ff1
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-15 05:42:38 +0300

    Lesson12

--- Task_12/Task_12-2.py
+++ Task_12/Task_12-2.py
@@ -5,9 +5,14 @@
 # которое генерирует следующую последовательность:
 # 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, и т.д. до 10
 
-lst = [x for x in range(1, 11) for y in range(x)]
+print(*[x for x in range(1, 11) for y in range(x)])
+# Пустой списолк
 # lst = []
+# #Цикл от 1 до 10 включительно
 # for x in range(1, 11):
+#     # Цикл от 0 до Х
 #     for y in range(x):
+#         # Прибавляем Х к списку
 #         lst.append(x)
-print(lst)
+# Вывод на экран списка
+# print(*lst)
