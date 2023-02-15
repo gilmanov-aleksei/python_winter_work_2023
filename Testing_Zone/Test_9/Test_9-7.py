9145b296dd84d3ec8316ebe1f5a2ea97bb755690
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-15 21:21:54 +0300

    TestFile

--- Testing_Zone/Test_9/Test_9-7.py
+++ Testing_Zone/Test_9/Test_9-7.py
@@ -1,1 +1,17 @@
 #! /usr/bin/python
+
+lst = [1, 10, 100, 2, 20, 200]
+
+
+# [1, 10, 100, 2, 20, 200]
+# 1, 11, 111, 113, 133, 333
+def su():
+    t = 0
+    for v in lst:
+        t += v
+        yield t
+
+
+gf = su()
+for x in gf:
+    print(x)
