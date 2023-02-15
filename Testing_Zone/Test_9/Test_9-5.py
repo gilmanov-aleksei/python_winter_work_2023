259f446649fe2e9d7ca776db8253a1cd8400e361
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-15 21:03:56 +0300

    TestFile

--- Testing_Zone/Test_9/Test_9-5.py
+++ Testing_Zone/Test_9/Test_9-5.py
@@ -1,1 +1,15 @@
 #! /usr/bin/python
+
+def fun(n):
+    for x in range(n):
+        yield x * x
+
+
+g = fun(3)
+print(next(g))
+print(next(g))
+print(next(g))
+try:
+    print(print(next(g)))
+except StopIteration:
+    print("Больше нет данных")
--- Testing_Zone/Test_9/Test_9-6.py
+++ Testing_Zone/Test_9/Test_9-6.py
@@ -1,1 +1,14 @@
 #! /usr/bin/python
+
+n = int(input())
+def factorial():
+    f, k = 1, 1
+    while True:
+        yield k, f
+        k += 1
+        f += k # f *= k
+
+
+gf = factorial()
+for _ in range(n):
+    print(next(gf))
