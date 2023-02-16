ff1986960b7c3b769ec41100135346afce8e19bd
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-16 08:52:48 +0300

    Lesson 13

--- Task_13/Task_13-2.py
+++ Task_13/Task_13-2.py
@@ -1,4 +1,6 @@
 #! /usr/bin/python
+import time
+
 
 # Задача 13-2
 # Создайте функцию-генератор,
@@ -7,16 +9,18 @@
 # 11 22 33 44 55 66 77 88 99
 # 101 111 121 131 141 151 161 171 181 191 202 212 ...
 
-# def fu():
-for i in range(1, 200):
-    a = str(i)
-    b = ''
-    for j in range(len(a) - 1, -1, -1):
-        b += a[j]
-    if a == b:
-        print(i, end=" ")
+def fu(v):
+    for j in range(1, v):
+        m = j
+        n = 0
+        while j > 0:
+            n = n * 10 + j % 10
+            j = j // 10
+        if m == n:
+            yield m
 
-# gf = fu()
-# for i in gf:
-#     print(i, end=", ")
-#     time.sleep(0.25)
+
+gf = fu(250)
+for i in gf:
+    print(i, end=", ")
+    time.sleep(0.25)
