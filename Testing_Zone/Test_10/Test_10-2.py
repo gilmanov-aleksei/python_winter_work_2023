ab7e8ff021a052939e0bf794d9cb90ac4e65fe09
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-17 19:06:34 +0300

    TestFile

--- Testing_Zone/Test_10/Test_10-2.py
+++ Testing_Zone/Test_10/Test_10-2.py
@@ -1,1 +1,24 @@
 #! /usr/bin/python
+
+# try:
+#     file = open('ok123.txt', 'r')
+# except FileNotFoundError as e:
+#     print(FileNotFoundError, e)
+# except Exception as e:
+#     print(Exception, e)
+#
+
+def divide(x, y):
+    try:
+        out = x / y
+    except:
+        try:
+            import math
+            out = math.inf * x / abs(x)
+        except:
+            out = None
+    finally:
+        return out
+
+
+print(divide(0, 0))
