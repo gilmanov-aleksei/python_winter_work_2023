b1ea822ffba4ef4afa883694ae0bfad13ddf8661
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-18 00:17:05 +0300

    Lesson 14

--- Task_14/Task_14-2.py
+++ Task_14/Task_14-2.py
@@ -2,3 +2,13 @@
 
 # Напишите рекурсивную функцияю,
 # которая вычисляет сумму цифр натурального числа
+
+def summ_nat(n):
+    if n == 0: return 0
+    return n % 10 + summ_nat(n // 10)
+
+
+print(summ_nat(int(input("Enter num: "))))
+
+
+
