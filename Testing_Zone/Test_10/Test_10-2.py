e6063c4e17d4df69f0f0d69cb62e355008fd9105
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-17 19:27:46 +0300

    TestFile

--- Testing_Zone/Test_10/Test_10-2.py
+++ Testing_Zone/Test_10/Test_10-2.py
@@ -8,17 +8,65 @@
 #     print(Exception, e)
 #
 
-def divide(x, y):
-    try:
-        out = x / y
-    except:
-        try:
-            import math
-            out = math.inf * x / abs(x)
-        except:
-            out = None
-    finally:
-        return out
+# def divide(x, y):
+#     try:
+#         out = x / y
+#     except:
+#         try:
+#             import math
+#             out = math.inf * x / abs(x)
+#         except:
+#             out = None
+#     finally:
+#         return out
+#
+#
+# print(divide(0, 0))
 
+# try:
+#     raise Exception("Что-то пошло не так")
+# except Exception as e:
+#     print("Message:" + str(e))
 
-print(divide(0, 0))
+# def validate(name):
+#     if len(name) < 10:
+#         raise ValueError
+#
+#
+# try:
+#     name = input("Enter Name:")
+#     validate(name)
+# except ValueError:
+#     print("Name is short")
+#
+# class NameTooShortError(ValueError):
+#     pass
+#
+#
+# def validate(name):
+#     if len(name) < 10:
+#         raise NameTooShortError
+#
+# class Positive(ValueError): pass
+#
+#
+# class Negative(ValueError): pass
+#
+#
+# def fun(lst):
+#     for x in lst:
+#         try:
+#             if x == 0:
+#                 print(0)
+#             elif x > 0:
+#                 raise Positive
+#             else:
+#                 raise Negative
+#         except Positive:
+#             print("Положительное")
+#         except Negative:
+#             print("Отрицательное")
+#
+#
+# lst = [0, -15, 1, 1.5, - 2.7, ]
+# print(fun(lst))
