7b1f377b420a49850b4a926d8a9690f0cf3114ea
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-17 23:07:42 +0300

    TestFile

---
+++ Task_14/Task_14-3-1.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+def triangle(n, m):
+    print(' ' * (m - n), '* ' * n)
+    if n < m:
+        triangle(n + 1, m)
+    # print(' ' * (m - n), '* ' * n)
+    return
+
+
+triangle(0, 10)
