1f460f9a1cc16f1835941872ab026e19187e3c65
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-17 20:55:52 +0300

    TestFile

--- Testing_Zone/Test_10/Test_10-4.py
+++ Testing_Zone/Test_10/Test_10-4.py
@@ -1,1 +1,28 @@
 #! /usr/bin/python
+
+# def func1(m, n):
+#     print("func1", n, m)
+#     n -= 1
+#     m += 1
+#     if n > 0:
+#         func1(m, n)
+#     print("------")
+#     print("func2", n, m)
+#     return
+#
+#
+# func1(0, 5)
+
+# def fact(n):
+#     # if n == 1:
+#     #     return 1
+#     # else:
+#     #     return n*fact(n-1)
+#     return 1 if n == 0 else n * fact(n-1)
+# print(fact(1))
+# print(fact(2))
+# print(fact(3))
+# print(fact(4))
+# print(fact(5))
+
+def triangle(n):
