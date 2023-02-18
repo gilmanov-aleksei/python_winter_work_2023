1c97695cddeebe2ec6fd89af329e44c259c68060
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-19 01:47:08 +0300

    Lesson 14

--- Task_14/Task_14-1.py
+++ Task_14/Task_14-1.py
@@ -5,10 +5,14 @@
 # цифры введенного натурального числа
 
 def num_len(n):
+    # Делим число на 10 без остатка
     n //= 10
+    # Базовый случай, если ноль,
     if n == 0:
+        # то возвращаяем 1
         return 1
+    # иначе, прибавляем на 1
     return 1 + num_len(n)
 
 
-print(num_len(abs(int(input("Enter the number: ")))))
+print(f"Number length:", num_len(abs(int(input("Enter the number: ")))))
