10900a8178f00ffa579a8f13945d5325c6164293
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-19 02:03:51 +0300

    Lesson 14

--- Task_05/Task_5-0.py
+++ Task_05/Task_5-0.py
@@ -22,28 +22,28 @@ def sort_word(v):
 
 
 # s = input("Enter a word: ")
-s = "appely"
+s = "appaale"
 d = 'aeiouy'
 
 # Разделение слова в список букв
-si = [y for w in s for y in w]
+# si = [y for w in s for y in w]
 
-glas = [x for x in (si) if x in d]
-soglas = [y for y in (si) if not y in d]
+glas = [x for x in s if x in d]
+soglas = [y for y in s if not y in d]
 
 lg = len(glas)
 ls = len(soglas)
-
-# sum(2 for letter in s if letter in vowels) > len(s)
-if lg == ls or lg + 1 == ls:
-    glas.append(' ')
-    glas.sort()
-    print(sort_word(lg))
-elif lg - 1 == ls:
-    soglas.append(' ')
-    print(sort_word(lg))
-else:
-    print("Impossible!")
-
 print(glas)
 print(soglas)
+
+# sum(2 for letter in s if letter in vowels) > len(s)
+if lg == ls:
+    print(s, "-->", sort_word(lg))
+elif lg + 1 == ls:
+    glas.append(" ")
+    print(s, "-->", sort_word(ls))
+elif lg - 1 == ls:
+    soglas.append(" ")
+    print(s, "-->", sort_word(lg))
+else:
+    print(s, "--> Impossible!")
