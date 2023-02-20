4e5fc2c5184b62ba00bbe41268377786db532722
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-20 21:11:56 +0300

    TestFile

--- Testing_Zone/Test_11/Test_11-3.py
+++ Testing_Zone/Test_11/Test_11-3.py
@@ -1,1 +1,10 @@
 #! /usr/bin/python
+
+string = "axR.456 987-456-3456 xdfs.4567 3456-234-2345 R45.d78 23.xuh 987-234-9845 987-234-9sd5 A178BC98 A783BC198"
+import re
+
+print(re.findall(r"\b\w\w\w[.]\w\w\w\b", string))
+print(re.findall(r"\b\d\d\d-\d\d\d-\d\d\d\d\b", string))
+print(re.findall(r"\b\d{3}-\d{3}-\d{4}", string))
+print(re.findall(r"\b[A-Z]\d{3}[A-Z]{2}\d{2,3}\b", string))
+print(re.findall(r"\b[A-Z]\d{3}[A-Z]{2}1?[7,9]8\b", string))
