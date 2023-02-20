9ce3d1a80e2e6738d61d6035ccd2281ec7dd7959
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-20 19:08:31 +0300

    TestFile

--- Testing_Zone/Test_11/Test_11-1.py
+++ Testing_Zone/Test_11/Test_11-1.py
@@ -1,1 +1,27 @@
 #! /usr/bin/python
+
+a = print(10)
+print(a or "a")
+print(a, "a")
+
+
+#! /usr/bin/python
+
+res = []
+
+
+def list_proc(lst):
+    print(lst)
+
+    for x in lst:
+        if type(x) == list:
+            list_proc(x)
+        else:
+            res.append(x)
+    print(res)
+    return res
+
+
+lst = [1, 2, [11, 22, [111, 222, [1111, 2222, 33333], 333, 444, [555, 666]], 3], 4]
+
+print(list_proc(lst))
