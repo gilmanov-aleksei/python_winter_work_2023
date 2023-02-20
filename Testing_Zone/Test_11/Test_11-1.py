b0e13b9a21f7e6b1314f963ae0c4f8591e3ce9ae
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-20 20:18:10 +0300

    TestFile

--- Testing_Zone/Test_11/Test_11-1.py
+++ Testing_Zone/Test_11/Test_11-1.py
@@ -1,31 +1,30 @@
 #! /usr/bin/python
 
-import sys
-print(sys.version)
-print(sys.getrecursionlimit())
+# import sys
+# print(sys.version)
+# print(sys.getrecursionlimit())
 
 a = print(10)
 print(a or "a")
 print(a, "a")
 
 
-#! /usr/bin/python
 
-res = []
-
-
-def list_proc(lst):
-    print(lst)
-
-    for x in lst:
-        if type(x) == list:
-            list_proc(x)
-        else:
-            res.append(x)
-    print(res)
-    return res
-
-
-lst = [1, 2, [11, 22, [111, 222, [1111, 2222, 33333], 333, 444, [555, 666]], 3], 4]
-
-print(list_proc(lst))
+# res = []
+#
+#
+# def list_proc(lst):
+#     print(lst)
+#
+#     for x in lst:
+#         if type(x) == list:
+#             list_proc(x)
+#         else:
+#             res.append(x)
+#     print(res)
+#     return res
+#
+#
+# lst = [1, 2, [11, 22, [111, 222, [1111, 2222, 33333], 333, 444, [555, 666]], 3], 4]
+#
+# print(list_proc(lst))
--- Testing_Zone/Test_11/Test_11-2.py
+++ Testing_Zone/Test_11/Test_11-2.py
@@ -4,6 +4,21 @@
 # import re
 # print(re.findall(r" \d{2},", st))
 
-st = "Широка страна моя родная"
-import re
-print(re.findall(r"\b\w+о\w+\b", st))
+# st = "Широка страна моя родная"
+# import re
+# print(re.findall(r"\b\w+о\w+\b", st))
+
+# import re
+# string = "0abracadabra1"
+# regex = r".a."
+# print(re.findall(regex, string))
+
+# import re
+# string = "100 200 1134 321 999 299 1999 199"
+# regex = r"\b1\d\d\b"
+# print(re.findall(regex, string))
+#
+# string = "Косой косой косил траву на косе"
+# import re
+# # print(re.findall(r"\b\w+\b", string))
+# print(re.findall(r"\b\w*[Кк]ос\w+\b", string))
