c4044c84f94c551a1d1809aa5fd66b9d6e9cf235
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-20 11:28:56 +0300

    Lesson 14

--- Task_14/Task_14-3-2.py
+++ Task_14/Task_14-3-2.py
@@ -1,20 +1,28 @@
-#! /usr/bin/python
-def prt_zv(m):
-    return print(('* ' * m).center(10))
+# #! /usr/bin/python
+#
+# def tri_2_2(n):
+#     if n > 1:
+#         print(('* ' * n).center(n - 2 * n))
+#         tri_2_2(n - 1)
+#     return print(('* ' * n).center(n - 2 * n))
+#
+#
+# def tri_2_3(n):
+#     if n > 0: print(('* ' * n).center(10)) or tri_2_3(n - 1) or print(('* ' * n).center(10))
+#     return
+#
+#
+# tri_2_2(5)
+# input("Press Enter")
+# tri_2_3(5)
+
+def tri(n):
+    if n == 1:
+        print('*'.center(10))
+    else:
+        print(('* ' * n).center(10))
+        tri(n - 1)
+        print(('* ' * n).center(10))
 
 
-def tri_2_2(n):
-    if n > 1:
-        prt_zv(n)
-        tri_2_2(n - 1)
-    return prt_zv(n)
-
-
-def tri_2_3(n):
-    if n > 1: prt_zv(n) or tri_2_3(n - 1) or prt_zv(n)
-    return
-
-
-tri_2_2(5)
-input("Press Enter")
-tri_2_3(5)
+tri(5)
