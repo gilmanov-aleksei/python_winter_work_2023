112bb39f55391c52fcd5b8093f4328430deb4d71
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-20 10:10:48 +0300

    Lesson 14

--- Task_14/Task_14-3-2.py
+++ Task_14/Task_14-3-2.py
@@ -2,15 +2,16 @@
 def prt_zvezda(m):
     return print(('* ' * m).center(10))
 
+
 def tri_2_2(n):
     if n > 1:
         prt_zvezda(n)
         tri_2_2(n - 1)
     return prt_zvezda(n)
 
+
 def tri_2_3(n):
-    if n > 0:
-        prt_zvezda(n) or tri_2_3(n - 1) or prt_zvezda(n)
+    if n > 1: prt_zvezda(n) or tri_2_3(n - 1) or prt_zvezda(n)
     return
 
 
