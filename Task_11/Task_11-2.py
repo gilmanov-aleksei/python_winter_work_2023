83726cdf549e38f53c4f19003470c0e9c347fe4a
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-22 02:14:57 +0300

    Lesson10

--- Task_11/Task_11-2.py
+++ Task_11/Task_11-2.py
@@ -12,20 +12,29 @@
 import csv
 import openpyxl
 
-itog = []
-
 with open('Task_11-2.csv') as f:
     reader = list(csv.reader(f, delimiter=','))
     begin = reader.pop(0)
-    print(begin)
-    print()
     reader.sort(key=lambda x: [x[3], x[1], x[2]])
-    for row in reader:
-        print(row)
 
 wb = openpyxl.Workbook()
 sheet = wb.active
 sheet.title = "Values"
-print(f'Active list: {wb.active.title}')
+ws = wb['Values']
 
+row = 1
+for k, v in enumerate(begin):
+    ws[str(chr(65 + k) + str(row))] = v
+row += 1
+itogo = 0
+colum = 0
+for m in reader:
+    itogo += int(''.join(map(str, m[-1:])))
+    for i, j in enumerate(m, 65):
+        ws[str(chr(i)) + str(row)] = j
+    row += 1
+colum = i
+ws['A' + str(row)] = 'ИТОГО'
+ws[str(chr(colum)) + str(row)] = itogo
 wb.save("Task_11-2.xlsx")
+
