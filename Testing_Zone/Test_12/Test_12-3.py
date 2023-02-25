bae0ffb004727c8dff41bba317098bf47ef123b9
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-25 22:05:10 +0300

    TestFile

--- Testing_Zone/Test_12/Test_12-3.py
+++ Testing_Zone/Test_12/Test_12-3.py
@@ -8,9 +8,21 @@ import re
 # text = 'Гильманов Алексей Гаптрауфович'
 # print(re.sub(r"(Гильманов) (Алексей) (Гаптрауфович)", r'\2 \3 \1', text))
 
+# import re
+#
+# text = 'Программы на Java транслируются в байт код java,' \
+#        'выполняемый виртуальной машиной Java (JVM) программой,' \
+#        'обрабатывающей байтовый код и передающей инструкции оборудованию как интерпретатор.'
+# res = re.sub(r'Java', r'Python', text, count=2, flags=re.I)
+# print(res)
+
+# import re
+#
+# text = '123 first 234 second'
+# print(re.findall(r"\d+(\w+)", text))
+
 import re
-text = 'Программы на Java транслируются в байт код java,' \
-       'выполняемый виртуальной машиной Java (JVM) программой,' \
-       'обрабатывающей байтовый код и передающей инструкции оборудованию как интерпретатор.'
-res = re.sub(r'Java', r'Python', text, count=2, flags=re.I)
+
+text = 'Миша:123 Коля:234 Сеня:345'
+res = re.findall(r"(\w+):(\d+)", text)
 print(res)
