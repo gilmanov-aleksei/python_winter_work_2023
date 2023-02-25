5166ba97725c4c4446a2b56aa0dc0eb83dd58bf3
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-25 22:21:04 +0300

    TestFile

--- Testing_Zone/Test_12/Test_12-4.py
+++ Testing_Zone/Test_12/Test_12-4.py
@@ -1,1 +1,15 @@
 #! /usr/bin/python
+#
+# import re
+#
+# text = 'abracadabra'
+# # res = re.finditer(r"a", text)
+# res = re.finditer(r"[^a]", text)
+# for i in res:
+#     print(i.group(), i.start(), i.end())
+
+# import re
+#
+# text = "1 + 2222 * 3 - 7 / 2"
+# # print(re.split(r"\s+", text))
+# print(re.split(r"\D+", text))
