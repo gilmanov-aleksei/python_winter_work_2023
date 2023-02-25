9f132fdb4b0a0c8b125b05d6126e59768ea3e951
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-25 21:36:18 +0300

    TestFile

--- Testing_Zone/Test_12/Test_12-2.py
+++ Testing_Zone/Test_12/Test_12-2.py
@@ -1,1 +1,15 @@
 #! /usr/bin/python
+
+# import re
+#
+# text = 'http://www.lksjflskj.ru'
+# res, n = re.subn(r'Java', r'Python', text)
+# print(res, n)
+
+import re
+
+text = 'fizz.123.buzz.456.fizzbuzz'
+res1 = re.sub(r'\d+', r'#', text)
+res2 = re.sub(r'[a z]+', r'(*)', text)
+print(res1)
+print(res2)
--- Testing_Zone/Test_12/Test_12_1.py
+++ Testing_Zone/Test_12/Test_12_1.py
@@ -1,19 +1,21 @@
 #! /usr/bin/python
 
 # import re
+#
 # st = 'but bot bit bite bottle rabbit bat batman'
-# print(re.findall(r"b.t", st)) # 1
-# print(re.findall(r"b.t", st)) # 2
-# print(re.findall(r"...", st)) # 3
-# print(re.findall(r"..t", st)) # 4
-# print(re.findall(r"\bb\b", st)) # 5
-# print(re.findall(r"b.*", st)) # 6
-# print(re.findall(r"b\w*b[aioeu]t\w*\w*\b", st)) # 7
-# print(re.findall(r"\w*b[aioeu]t\w*\w*", st)) # 8
-# print(re.findall(r"\w*b[aioeu]t\w*\w*\w*", st)) # 9
-# print(re.findall(r"\w*b[aioeu]t\w*\w*\w*\w*", st)) # 10
+# print(re.findall(r"b.t", st))  # 1
+# print(re.findall(r"b.t", st))  # 2
+# print(re.findall(r"...", st))  # 3
+# print(re.findall(r"..t", st))  # 4
+# print(re.findall(r"\bb\b", st))  # 5
+# print(re.findall(r"b.*", st))  # 6
+# print(re.findall(r"b\w*b[aioeu]t\w*\w*\b", st))  # 7
+# print(re.findall(r"\w*b[aioeu]t\w*\w*", st))  # 8
+# print(re.findall(r"\w*b[aioeu]t\w*\w*\w*", st))  # 9
+# print(re.findall(r"\w*b[aioeu]t\w*\w*\w*\w*", st))  # 10
 
 import re
+
 text = 'Java самый популярный язык программирования в 2023 году.'
 res = re.sub(r'Java', r'Python', text)
 print(res)
