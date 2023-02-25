97e16e9714826d6c2a66a4463a6561f89fd3cfeb
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-25 21:59:30 +0300

    TestFile

--- Testing_Zone/Test_12/Test_12-3.py
+++ Testing_Zone/Test_12/Test_12-3.py
@@ -1,1 +1,16 @@
 #! /usr/bin/python
+
+import re
+
+# text = 'first second'
+# print(re.sub(r"(first) (second)", r'\2 \1', text))
+
+# text = 'Гильманов Алексей Гаптрауфович'
+# print(re.sub(r"(Гильманов) (Алексей) (Гаптрауфович)", r'\2 \3 \1', text))
+
+import re
+text = 'Программы на Java транслируются в байт код java,' \
+       'выполняемый виртуальной машиной Java (JVM) программой,' \
+       'обрабатывающей байтовый код и передающей инструкции оборудованию как интерпретатор.'
+res = re.sub(r'Java', r'Python', text, count=2, flags=re.I)
+print(res)
