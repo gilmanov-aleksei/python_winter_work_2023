c37e2e10e469436faf33f1711d10a0cac06e2f15
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-25 23:08:47 +0300

    TestFile

--- Testing_Zone/Test_12/Test_12-6.py
+++ Testing_Zone/Test_12/Test_12-6.py
@@ -1,2 +1,44 @@
 #! /usr/bin/python
 
+# def null_decorator(func):
+#     return func  # Возвращает саму функцию
+#
+#
+# def hello():
+#     print('Hello world!')
+#
+#
+# hello = null_decorator(hello)  # декорируем функцию hello
+# hello()
+
+# def sample_decorator(func):  # определяем декоратор
+#     def wrapper():
+#         print('Начало функции')
+#         func()
+#         # мы обертываем функцию func , не вмешиваясь
+#         # в ее работу
+#         print('Конец функции')
+#
+#     return wrapper
+#
+#
+# def say():
+#     print('Привет Мир!')
+#
+#
+# say = sample_decorator(say)  # декорируем функцию
+# say()  # вызываем декорированную функцию
+
+def uppercase_decorator(func):
+    def wrapper():
+        original_result = func()
+        modified_result = original_result.upper()
+        return modified_result
+    return wrapper
+
+# @uppercase_decorator
+def h():
+    return 'Hello'
+
+# h = uppercase_decorator(h)
+print(h())
