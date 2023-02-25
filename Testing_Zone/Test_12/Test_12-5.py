ae8e7b289e7fdf702f9677156e74ca49cd123b28
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-25 22:50:43 +0300

    TestFile

--- Testing_Zone/Test_12/Test_12-5.py
+++ Testing_Zone/Test_12/Test_12-5.py
@@ -1,1 +1,47 @@
 #! /usr/bin/python
+
+
+# def func(f):
+#     return f
+#
+#
+# def hello():
+#     print('Привет!!!')
+#
+#
+# hello()
+# func(hello)()
+
+# def speak(text):
+#     def whisper(t):
+#         return t.lower() + '...'
+#
+#     return whisper(text)
+#
+#
+# print(speak('Hello, World'))
+
+# def speak():
+#     def whisper(t):
+#         return t.lower() + '...'
+#
+#     return whisper
+#
+#
+# # Получаем из функции speak объект функции whisper
+# wr = speak()
+# # Вызываем функцию с одним аргументом
+# print(wr('Hello, World'))
+
+def speak(text):
+    def whisper():
+        print(text.lower() + '...')
+
+    return whisper
+
+
+foo = speak('Hello World')
+bar = speak('Wellcome')
+
+foo()
+bar()
