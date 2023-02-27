ca33a9741d8969d45dcd474ac5bc5024706c3465
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-27 23:54:22 +0300

    Lesson 17

--- Task_17/Task_17-3.py
+++ Task_17/Task_17-3.py
@@ -9,21 +9,47 @@
 # 4. Запросить площадь объекта
 
 class Shape:
-    @classmethod
     def __init__(self, colour, square):
         self.colour = colour
         self.square = square
 
-    def instsl_c(self, c):
-        pass
+    def info(self):
+        print(f'Цвет объекта: {self.colour}. Его площадь: {self.square} кв.мм')
 
-    def get_c_print(self, c):
-        pass
+    def instsl_colour(self):
+        if self.colour == 'Blue':
+            self.colour = 'Red'
+        elif self.colour == 'Red':
+            self.colour = 'Green'
+        else:
+            self.colour = 'Blue'
 
-    def instal_s(self, s):
-        pass
+    def get_colour_print(self):
+        obj_colour = self.colour
+        print(obj_colour)
 
-    def get_square(self, s):
-        pass
+    def instal_square(self):
+        self.square += 150
+
+    def get_square(self):
+        obj_square = self.square
+        print(obj_square)
 
 
+b = Shape('Blue', 100)
+r = Shape('Red', 200)
+
+b.info()
+r.info()
+
+b.instsl_colour()
+r.instsl_colour()
+
+b.get_colour_print()
+r.get_colour_print()
+
+b.instal_square()
+r.instal_square()
+
+b.get_square()
+r.get_square()
