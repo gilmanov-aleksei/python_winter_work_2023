a79ab45bbdb5b76c5d6a384faf6abdbe4c7cd5c9
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-27 23:25:44 +0300

    Lesson 17

--- Task_17/Task_17-3.py
+++ Task_17/Task_17-3.py
@@ -1,2 +1,29 @@
 #! /usr/bin/python
 
+# Создайте класс Shape, объекты которого имеют атрибуты Colour -
+# строка, например, "Красный", "Синий"; Square - площадь объекта
+# Создайте несколько методов:
+# 1. Установить цвет объекта
+# 2. Запросить цвет объекта и напечатать его
+# 3. Задать площадь объекта
+# 4. Запросить площадь объекта
+
+class Shape:
+    @classmethod
+    def __init__(self, colour, square):
+        self.colour = colour
+        self.square = square
+
+    def instsl_c(self, c):
+        pass
+
+    def get_c_print(self, c):
+        pass
+
+    def instal_s(self, s):
+        pass
+
+    def get_square(self, s):
+        pass
+
+
