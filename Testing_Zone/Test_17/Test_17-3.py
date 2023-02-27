e1644e57b0931ecf29bebb9a101701a9ce382179
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-27 20:53:06 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-3.py
+++ Testing_Zone/Test_17/Test_17-3.py
@@ -1,2 +1,38 @@
 #! /usr/bin/python
 
+class Person:
+    def __init__(self, name, money):
+        self.name = name
+        self.money = money
+        self.cur_money = self.money
+
+    def info(self):
+        print(self.name, self.cur_money)
+
+    def set_money(self, x):
+        self.money += x
+
+    def get_money(self):
+        return self.cur_money
+
+    def give_money(self, other, x):
+        if self.cur_money >= x:
+            other.money += x
+            self.cur_money -= x
+        else:
+            print("No money!")
+
+
+
+
+a = Person('Pete', 200)
+b = Person('Nick', 300)
+c = Person('Jack', 500)
+
+
+a.info(), b.info(), c.info()
+c.give_money(a, 200)
+c.give_money(b, 200)
+a.info(), b.info(), c.info()
+c.give_money(b, 200)
+a.info(), b.info(), c.info()
