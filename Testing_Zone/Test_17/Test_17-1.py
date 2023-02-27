aba4b587894e6a734c22c66fb3573c1a0cb69d73
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-27 19:27:21 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-1.py
+++ Testing_Zone/Test_17/Test_17-1.py
@@ -1,2 +1,24 @@
 #! /usr/bin/python
 
+# import re
+#
+# print(re.findall(r'\b.*\b', 'first second third'))
+# print(re.findall(r'\b.*?\b', 'first second third'))
+
+# import re
+#
+# podstr = input("Input: ")
+# s = "Косой косой косил волос на осине"
+# print(re.findall(rf'\b\w+{podstr}\w+\b', s))
+
+# def fu(*args, **kwargs):
+#     res = ""
+#     for x in args:
+#         if type(x) == str:
+#             res += x
+#     for x in kwargs:
+#         if type(kwargs[x]) == str:
+#             res += kwargs[x]
+#     return res
+#
+# print(fu(1, 2, 'abc', a=3, b='def'))
