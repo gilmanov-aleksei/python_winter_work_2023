c4b38f8bfe1ce8d9d540074e2027b84c5ab1396b
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-27 20:15:48 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-2.py
+++ Testing_Zone/Test_17/Test_17-2.py
@@ -1,26 +1,47 @@
 #! /usr/bin/python
 
 
-import time
+#
+# import time
+#
+# def timer(func):
+#     def wrapper(*args, **kwargs):
+#         start = time.perf_counter()
+#         val = func(*args, **kwargs)
+#         end = time.perf_counter()
+#         work_time = end - start
+#         print(f'Время выполнения {func.__name__}:{round(work_time, 4)} сек.')
+#         return val
+#
+#     return wrapper
+#
+#
+# @timer
+# def test(n): return sum([i ** 2 for i in range(n)])
+# @timer
+# def sleep(n): time.sleep(n)
+#
+# res1 = test(10000)
+# res2 = sleep(4)
+# print(f'Результат функции test = {res1}')
+# print(f'Результат функции test = {res2}')
 
-def timer(func):
+def decorator(func):
     def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        val = func(*args, **kwargs)
-        end = time.perf_counter()
-        work_time = end - start
-        print(f'Время выполнения {func.___name___}:{round(work_time, 4)} сек.')
-        return val
+        z = func(*args)
+        res = ''.join(args)
+        return res
 
-    return wrapper()
+    return wrapper
 
 
-@timer
-def test(n): return sum([(i / 99) ** 2 for i in range(n)])
-@timer
-def sleep(n): time.sleep(n)
-
-res1 = test(10000)
-res2 = sleep(4)
-print(f'Результат функции test = {res1}')
-print(f'Результат функции test = {res2}')
+@decorator
+def aaa(*args):
+    print(aaa.__name__)
+    return
+print(aaa('xxx', 'yyy', 'zzz'))
+@decorator
+def bbb(x, y):
+    print(bbb.__name__)
+    return len(x) + len(y)
+print(bbb('Hello', 'world'))
