9767d490cd1e802fe110cca42fffb32ef8beab56
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-27 19:44:08 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-2.py
+++ Testing_Zone/Test_17/Test_17-2.py
@@ -1,2 +1,26 @@
 #! /usr/bin/python
 
+
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        val = func(*args, **kwargs)
+        end = time.perf_counter()
+        work_time = end - start
+        print(f'Время выполнения {func.___name___}:{round(work_time, 4)} сек.')
+        return val
+
+    return wrapper()
+
+
+@timer
+def test(n): return sum([(i / 99) ** 2 for i in range(n)])
+@timer
+def sleep(n): time.sleep(n)
+
+res1 = test(10000)
+res2 = sleep(4)
+print(f'Результат функции test = {res1}')
+print(f'Результат функции test = {res2}')
