40bcbf832769ccce14e34c5eb2e2545636183ff7
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-02-28 00:41:44 +0300

    Lesson 17

--- Task_17/Task_17-2.py
+++ Task_17/Task_17-2.py
@@ -3,3 +3,17 @@
 # Создайте декоратор, который переводит все
 # текстовые аргументы функции в верхний регистр
 # и возвращаетих в виде списка текстовых аргументов.
+
+
+def fu(*args, **kwargs):
+    res = []
+    for x in args:
+        if type(x) == str:
+            res.append(x)
+    for x in kwargs:
+        if type(kwargs[x]) == str:
+            res.append(kwargs[x])
+    return [y.upper() for y in res]
+
+
+print(fu(1, 2, 'abc', a=3, b='def'))
