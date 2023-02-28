f4ef6b7e40999df01308518bde2c160bbeaea194
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 00:10:08 +0300

    Lesson 17

--- Task_17/Task_17-2.py
+++ Task_17/Task_17-2.py
@@ -5,15 +5,18 @@
 # и возвращает их в виде списка текстовых аргументов.
 
 
-def fu(*args, **kwargs):
-    res = []
-    for x in args:
-        if type(x) == str:
-            res.append(x)
-    for x in kwargs:
-        if type(kwargs[x]) == str:
-            res.append(kwargs[x])
-    return [y.upper() for y in res]
+def fu1(*args, **kwargs):
+    def fu2(*args, **kwargs):
+        res = []
+        for x in args:
+            if type(x) == str:
+                res.append(x)
+        for x in kwargs:
+            if type(kwargs[x]) == str:
+                res.append(kwargs[x])
+        return [y.upper() for y in res]
+    return fu2(*args, **kwargs)
 
 
-print(fu(1, 3, 6, 8, 'qwerty', a=3, b='def', v='test', z='plkm'))
+print(fu1(1, 3, 6, 8, 'qwerty', a=3, b='def', v='test', z='plkm'))
+
