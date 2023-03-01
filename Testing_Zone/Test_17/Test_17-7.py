d4108db7fff02b987ab92c904e0b991423d508c7
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 19:50:00 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-7.py
+++ Testing_Zone/Test_17/Test_17-7.py
@@ -1,16 +1,37 @@
 #! /usr/bin/python
+#
+# class Student:
+#     lst = []
+#     def __init__(self, name):
+#         self.name = name
+#         Student.lst.append(self.name)
+#
+#
+# s1 = Student('Emma')
+# s1.name
+# s2 = Student('Nick')
+# print(Student.lst)
+# print(isinstance(s1, Student))
+# print(s1.__class__)
+# print(dir(s1))
 
-class Student:
-    lst = []
-    def __init__(self, name):
-        self.name = name
-        Student.lst.append(self.name)
-
-
-s1 = Student('Emma')
-s1.name
-s2 = Student('Nick')
-print(Student.lst)
-print(isinstance(s1, Student))
-print(s1.__class__)
-print(dir(s1))
+class Tree(object):
+    def __init__(self, kind, height):
+        self.kind = kind
+        self.age = 0
+        self.height = height
+    def grow(self):
+        self.age += 1
+class FruitTree(Tree):
+    def __init__(self, kind, height):
+        super().__init__(kind,height)
+    def give_fruits(self):
+        print('Collected 20kg of {}s'.format(self.kind))
+f_tree = FruitTree('apple', 0.7)
+f_tree.col = 'Red'
+print(f_tree.kind, f_tree.age, f_tree.height, f_tree.col)
+s_tree = Tree('oak', 1)
+s_tree.grow()
+print(s_tree.kind, s_tree.age, s_tree.height)
+# f_tree.give_fruits()
+# f_tree.grow()
