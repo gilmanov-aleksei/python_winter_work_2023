9a4d26bc5da88205a57ab287d72f297217f75a1b
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 19:10:42 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-7.py
+++ Testing_Zone/Test_17/Test_17-7.py
@@ -1,2 +1,16 @@
 #! /usr/bin/python
 
+class Shape:
+    def __init__(self, per, col):
+        self.per = per
+        self.col = col
+    def peri(self):
+        return self.per
+    def __str__(self):
+        return self.col + str(self.per)
+
+s = Shape(10, 'Red')
+print(s.per, s.col)
+z = Shape(20, 'Blue')
+print(z)
+
