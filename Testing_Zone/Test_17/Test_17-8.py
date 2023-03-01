89e75b0bf4d3b683feb77d67a830ebf9981b14e1
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 19:13:17 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-8.py
+++ Testing_Zone/Test_17/Test_17-8.py
@@ -1,2 +1,11 @@
 #! /usr/bin/python
 
+class Student:
+    def __init__(self, name):
+        print("inside constructor")
+        self.name = name
+        print("object inittalized")
+    def show(self):
+        print(self.name)
+
+s1 = Student('Emma')
