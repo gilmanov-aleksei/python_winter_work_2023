14baef677077d105a4cc3ba28008b7423b695f31
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 20:08:27 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-8.py
+++ Testing_Zone/Test_17/Test_17-8.py
@@ -1,2 +1,15 @@
 #! /usr/bin/python
 
+class Shape:
+    def __init__(self, color, per):
+        self.color = color
+        self.per = per
+    def peri(self):
+        return self.per
+class Triangle(Shape):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def peri(self):
+        return self.x + self.y + self.z
