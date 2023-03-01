bc3878e6e782fb5d5713a4939efccd3649e29528
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 21:35:19 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-8.py
+++ Testing_Zone/Test_17/Test_17-8.py
@@ -1,33 +1,33 @@
 #! /usr/bin/python
-
-class Shape:
-    def __init__(self, col = 'Red', per = 0):
-        self.col = col
-        self.per = per
-    def peri(self):
-        return self.per
-class Triangle(Shape):
-    def __init__(self, x, y, z):
-        super().__init__()
-        self.x = x
-        self.y = y
-        self.z = z
-    def peri(self):
-        return self.x + self.y + self.z
-class Rectangle(Shape):
-    def __init__(self, a, b):
-        super().__init__()
-        self.a = a
-        self.b = b
-    def peri(self):
-        return 2 * (self.a + self.b)
-class Square(Rectangle):
-    def __init__(self, side):
-        super().__init__(side, side)
-
-
-s = Square(7)
-print(s.peri())
+#
+# class Shape:
+#     def __init__(self, col = 'Red', per = 0):
+#         self.col = col
+#         self.per = per
+#     def peri(self):
+#         return self.per
+# class Triangle(Shape):
+#     def __init__(self, x, y, z):
+#         super().__init__()
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#     def peri(self):
+#         return self.x + self.y + self.z
+# class Rectangle(Shape):
+#     def __init__(self, a, b):
+#         super().__init__()
+#         self.a = a
+#         self.b = b
+#     def peri(self):
+#         return 2 * (self.a + self.b)
+# class Square(Rectangle):
+#     def __init__(self, side):
+#         super().__init__(side, side)
+#
+#
+# s = Square(7)
+# print(s.peri())
 
 
 # s = Shape('Blue', 5)
@@ -36,3 +36,36 @@ print(s.peri())
 # print(t.peri(), t.col)
 # r = Rectangle(10, 20)
 # print(r.peri(), r.col)
+#
+# class Horse:
+#     isHorse = True
+#     def Igogo(self): return 'Igogo'
+# class Donkey:
+#     isDonkey = True
+#     def Iaia(self): return 'Iaia'
+# class Mule(Horse, Donkey):
+#     pass
+# mu = Mule
+# print(mu.isHorse)
+# print(mu.isDonkey)
+
+#
+# class Triangle():
+#     def tr_check(x, y, z):
+#         if x > 0 and y > 0 and z > 0:
+#             if x + y > z and y + z > x and z + x > y:
+#                 return True
+#         return False
+#
+#     def __init__(self, x, y, z):
+#
+#         if Triangle.tr_check(x, y, z):
+#             self.x = x
+#             self.y = y
+#             self.z = z
+#         else:
+#             print('NO!!!')
+#
+#
+# t = Triangle(3, 4, 5)
+# print(t.)
--- Testing_Zone/Test_17/Test_17-9.py
+++ Testing_Zone/Test_17/Test_17-9.py
@@ -1,2 +1,53 @@
 #! /usr/bin/python
 
+# class X(str):
+#     def __init__(self, s):
+#         self.s = s
+#     def __add__(self, other):
+#         return other.s + self.s
+# class Y()
+
+# class SomeClass():
+#     def __init__(self):
+#         self.__param = 42
+#
+# obj = SomeClass()
+# # obj.__param
+# obj._SomeClass__param
+
+class Date:
+    def __init__(self, *info):
+        self.info = list(info)
+
+    def __getitem__(self, i):
+        return self.info[i]
+
+
+class Teacher:
+    def __init__(self):
+        self.work = 0
+
+    def teach(self, info, *pupil):
+        for i in pupil:
+            i.take(info)
+            self.work += 1
+
+
+class Pupil:
+    def __init__(self):
+        self.knowledge = []
+
+    def take(self, info):
+        self.knowledge.append(info)
+
+
+lesson = Date('class', 'object', 'inheritance', 'polymorphism', 'encapsulation')
+marivanna = Teacher()
+vasy = Pupil()
+pety = Pupil()
+
+marivanna.teach(lesson[2], vasy, pety)
+marivanna.teach(lesson[0], pety)
+
+print('Vasy', vasy.knowledge)
+print('Pety', pety.knowledge)
