fc2e561c95224b35b3fc7550d89e98eb397e78b7
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 19:17:55 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-8.py
+++ Testing_Zone/Test_17/Test_17-8.py
@@ -2,10 +2,22 @@
 
 class Student:
     def __init__(self, name):
-        print("inside constructor")
+        print('inside constructor')
         self.name = name
-        print("object inittalized")
+        print('object inittalized ' + self.name)
+
     def show(self):
         print(self.name)
 
+    def __del__(self):
+        print('inside destructor')
+        print('object destroyed')
+
+
 s1 = Student('Emma')
+s2 = Student('Nick')
+input()
+del s2
+input()
+s1.show()
+s2.show()
