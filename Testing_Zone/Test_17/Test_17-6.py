029c83ca5e8e748cc7d67a005165bafd25c57d8f
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 19:19:55 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-6.py
+++ Testing_Zone/Test_17/Test_17-6.py
@@ -8,3 +8,42 @@
 # print((11 > 0) is True)
 # print((0 < 0) == 0)
 # print((1 in range(2)) == True)
+
+#
+# class Shape:
+#     def __init__(self, per, col):
+#         self.per = per
+#         self.col = col
+#     def peri(self):
+#         return self.per
+#     def __str__(self):
+#         return self.col + str(self.per)
+#
+# s = Shape(10, 'Red')
+# print(s.per, s.col)
+# z = Shape(20, 'Blue')
+# print(z)
+#
+
+
+class Student:
+    def __init__(self, name):
+        print('inside constructor')
+        self.name = name
+        print('object inittalized ' + self.name)
+
+    def show(self):
+        print(self.name)
+
+    def __del__(self):
+        print('inside destructor')
+        print('object destroyed')
+
+
+s1 = Student('Emma')
+s2 = Student('Nick')
+input()
+del s2
+input()
+s1.show()
+s2.show()
