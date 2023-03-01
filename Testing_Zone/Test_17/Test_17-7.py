7035aae5e696cff82274a22d8b0575092252d702
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-01 19:37:51 +0300

    TestFile

--- Testing_Zone/Test_17/Test_17-7.py
+++ Testing_Zone/Test_17/Test_17-7.py
@@ -1,1 +1,16 @@
 #! /usr/bin/python
+
+class Student:
+    lst = []
+    def __init__(self, name):
+        self.name = name
+        Student.lst.append(self.name)
+
+
+s1 = Student('Emma')
+s1.name
+s2 = Student('Nick')
+print(Student.lst)
+print(isinstance(s1, Student))
+print(s1.__class__)
+print(dir(s1))
