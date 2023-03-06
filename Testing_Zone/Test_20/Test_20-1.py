5b3eb3d0ff906c8f16d5916b0bf24427d7abded4
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-06 19:00:29 +0300

    TestFile

--- Testing_Zone/Test_20/Test_20-1.py
+++ Testing_Zone/Test_20/Test_20-1.py
@@ -1,2 +1,20 @@
 #! /usr/bin/python
 
+# a = {True: '1', 1: 'one'}
+# print(a)
+# a = {1: '1', True: 'one'}
+# print(a)
+# a = {False: '0', 0: 'zero'}
+# print(a)
+# a = {0: '0', False: 'zero'}
+# print(a)
+
+import itertools
+
+# for j in range(1, 4, 1):
+#     for x in itertools.combinations([1, 2, 3, 4], j):
+#         print(x, end='')
+
+print(list(zip([1,2,3], [11,22,33,44])))
+
+print(list(itertools.zip_longest([1,2,3], [11,22,33,44], [111, 222, 333, 444, 555])))
