2cebc037237ddf48609e7e2b95a4fe31c364a350
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-07 14:58:42 +0300

    Lesson 19

--- Task_19/TAsk_19-1.py
+++ Task_19/TAsk_19-1.py
@@ -5,19 +5,27 @@
 # каждой купюры по одной штуке.
 # Какие суммы можно составить, если использовать по три купюры из них?
 
+# while True:
+#     n = input("Введите 3 и более числа через пробел и нажите Enter, 0 - выход: ").split()
+#     if '0' in n:
+#         break
+#     l = len(n)
+#     if l < 3:
+#         print("Введено меньше 3 чисел, повторите ввод.")
+#     else:
+#         n = list(map(int, n))
+#         s = []
+#         for i in range(l):
+#             for j in range(i + 1, l):
+#                 for k in range(j + 1, l):
+#                     s.append(n[i] + n[j] + n[k])
+#         print("Суммы трех чисел из списка:", s)
 
-while True:
-    n = input("Введите 3 и более числа через пробел и нажите Enter, 0 - выход: ").split()
-    if '0' in n:
-        break
-    l = len(n)
-    if l < 3:
-        print("Введено меньше 3 чисел, повторите ввод.")
-    else:
-        n = list(map(int, n))
-        s = []
-        for i in range(l):
-            for j in range(i + 1, l):
-                for k in range(j + 1, l):
-                    s.append(n[i] + n[j] + n[k])
-        print("Суммы трех чисел:", s)
+n = [50, 100, 200, 500, 1000, 5000]
+s = []
+for i in range(len(n)):
+    for j in range(i + 1, len(n)):
+        for k in range(j + 1, len(n)):
+            s.append(n[i] + n[j] + n[k])
+print(*n)
+print(*s)
