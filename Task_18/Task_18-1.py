7227bcf1a731fbf1a63f0cbcdaba9ee01ce7f136
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-07 10:50:28 +0300

    Lesson 18

--- Task_18/Task_18-1.py
+++ Task_18/Task_18-1.py
@@ -14,3 +14,155 @@
 # Отчетность? Запросы? Начните с формулировки решаемой задачи спецификации
 # или технического задания. Затем спроектируйте классы, атрибуты, методы.
 # Протестируйте систему.
+
+
+
+class Teacher:
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+        self.lessons = []
+
+    def add_lesson(self, lesson):
+        self.lessons.append(lesson)
+
+    def delete_lesson(self, lesson):
+        self.lessons.remove(lesson)
+
+    def view_lessons(self):
+        for lesson in self.lessons:
+            print(f"Lesson {lesson.lesson_number}")
+
+    def view_lesson(self, lesson_number):
+        for lesson in self.lessons:
+            if lesson.lesson_number == lesson_number:
+                return lesson
+        print("Урок не найден")
+        return None
+
+
+class Pupil:
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def delete_task(self, task):
+        self.tasks.remove(task)
+
+    def view_tasks(self):
+        for task in self.tasks:
+            print(f"Задание {task.number}: {task.status}")
+
+    def view_task(self, number):
+        for task in self.tasks:
+            if task.number == number:
+                return task
+        print("Задание не найдено")
+        return None
+
+
+class Lesson:
+    def __init__(self, lesson_number):
+        self.lesson_number = lesson_number
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def delete_task(self, task):
+        self.tasks.remove(task)
+
+    def view_tasks(self):
+        for task in self.tasks:
+            print(f"Задание {task.number}: {task.status}")
+
+    def view_task(self, number):
+        for task in self.tasks:
+            if task.number == number:
+                return task
+        print("Задание не найдено")
+        return None
+
+
+class Task:
+    def __init__(self, number, description):
+        self.number = number
+        self.description = description
+        self.status = "not solved"
+
+
+teacher = Teacher("Михаил", "Python Developer")
+pupil = Pupil("Алексей")
+
+lesson1 = Lesson(1)
+lesson2 = Lesson(2)
+task1 = Task(1, "Write a program to calculate the sum of two numbers")
+task2 = Task(2, "Write a program to calculate the product of two numbers")
+
+lesson1.add_task(task1)
+lesson1.add_task(task2)
+teacher.add_lesson(lesson1)
+teacher.add_lesson(lesson2)
+
+while True:
+    user_input = int(input("Введите число: 1 - Учитель, 2 - Ученик, 0 - Выход: "))
+    if user_input == 1:
+        teacher_input = int(
+            input("Введите число: 1 - посмотреть уроки, 2 - посмотреть урок по номеру, "
+                  "3 - добавить урок, 4 - удалить урок, 0 - вернуться в меню: ")
+        )
+        if teacher_input == 1:
+            teacher.view_lessons()
+        elif teacher_input == 2:
+            lesson_number = int(
+                input("Введите номер урока для просмотра: ")
+            )
+            lesson = teacher.view_lesson(lesson_number)
+            if lesson:
+                lesson.view_tasks()
+        elif teacher_input == 3:
+            lesson_number = int(
+                input("Введите ноер урока чтобы добавить: ")
+            )
+            lesson = Lesson(lesson_number)
+            teacher.add_lesson(lesson)
+        elif teacher_input == 4:
+            lesson_number = int(
+                input("Введите номер урока для удаления: ")
+            )
+            lesson = teacher.view_lesson(lesson_number)
+            if lesson:
+                teacher.delete_lesson(lesson)
+    elif user_input == 2:
+        pupil_input = int(
+            input("Введите число: 1 - для просмотра заданий, 2 - для просмотра задания по номеру, "
+                  "3 - добавить задание, 4 - удалить задание, 0 - вернуться в меню: ")
+        )
+        if pupil_input == 1:
+            pupil.view_tasks()
+        elif pupil_input == 2:
+            task_number = int(
+                input("Введите номер задания для просмотра: ")
+            )
+            task = pupil.view_task(task_number)
+            if task:
+                print(f"Задание {task.number}: {task.description}, {task.status}")
+        elif pupil_input == 3:
+            task_number = int(
+                input("Введит номер для добавления задания: ")
+            )
+            task_description = input("Введите номер задания для просмотра: ")
+            task = Task(task_number, task_description)
+            pupil.add_task(task)
+        elif pupil_input == 4:
+            task_number = int(
+                input("Введите номер задания для удаления: ")
+            )
+            task = pupil.view_task(task_number)
+            if task:
+                pupil.delete_task(task)
+    elif user_input == 0:
+        break
