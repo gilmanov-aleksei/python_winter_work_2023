4252e68f06c8ba71c8f58cf7991b795fe608a2f2
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-08 23:27:12 +0300

    Lesson 20

--- Task_20/Task_20-3.py
+++ Task_20/Task_20-3.py
@@ -5,28 +5,26 @@
 # циклическую последовательность из чисел и больших латинский букв.
 # 1,A,2,B,3,C,...,X,25,Y,26,Z,1,A,2,B,3,C,X,25,Y,26,Z
 
-class InfSequence:
-    def __init__(self):
-        self.index = 0
-        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+class InfSeq:  # Создаём класс бесконечный цикл
+    def __init__(self):  # Инициализация
+        self.index = 0  # Устанавливаем индекс на 0
+        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # Алфавит
 
-    def __iter__(self):
+    def __iter__(self):  # Итератор
         return self
 
     def __next__(self):
-        self.index += 1
-        if self.index == 54:
-            self.index = 1
-            return 'A'
-        elif self.index > 52:
-            self.index = 1
-        if self.index % 2 == 0:
-            return self.index // 2
+        self.index += 1  # Увеличиваем индекс на 1
+        if self.index >= 53:  # Если индекс достиг предела,
+            self.index = 1  # то сбрасываем его на 1
+            return 'A'  # и возвращаем букву А
+        if self.index % 2 == 0:  # проверка на чётность
+            return self.index // 2  # если четное, печатаем цифру
         else:
-            return self.alphabet[self.index // 2]
+            return self.alphabet[self.index // 2]  # если не четное, то букву
 
 
-seq = InfSequence()
+iseq = InfSeq()
 
 for i in range(57):
-    print(next(seq), end=',')
+    print(next(iseq), end=',')
