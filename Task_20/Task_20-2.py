8650fd488b1a62374a25db99c2c30baf6143f726
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-09 13:12:39 +0300

    Lesson 20

--- Task_20/Task_20-2.py
+++ Task_20/Task_20-2.py
@@ -21,30 +21,3 @@ df = pd.DataFrame(
 
 print(sum_numbers(df))
 print(df)
-
-#
-# import pandas as pd
-#
-#
-# def sum_num(df1, axis=0):
-#     num = df1.select_dtypes(include=['number']).columns
-#     return df1[num].sum(axis=axis).sum()
-#
-#
-# df1 = pd.DataFrame(
-#     [[8, 'abc', 5, 'cba', 3],
-#      [2, 'def', 3, 'fed', 7],
-#      [9, 'xyz', 4, 'zyx', 2]],
-#     columns=['A', 'B', 'C', 'D', 'E'])
-#
-# print(df1)
-#
-# # Сумма значений по колонкам
-# # сумма по столбцам
-# print(df1.sum())
-# # Сумма значений по строкам
-# # сумма по строкам
-# print(df1.sum(axis=1))
-# # Сумма значений по всей таблице
-# # сумма по строкам и столбцам
-# print(df1.sum(axis=0).sum(axis=0))
