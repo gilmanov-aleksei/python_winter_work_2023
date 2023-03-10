bfdd2b55d95c3814707c2e66c5bc2716a3d4d655
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-10 21:43:07 +0300

    TestFile

--- Testing_Zone/Test_20/Test_20-5.py
+++ Testing_Zone/Test_20/Test_20-5.py
@@ -13,5 +13,31 @@ for i in range(2001, 2006):
         df.iloc[x] = [i, j, k, m]
         x += 1
 df['Итого'] = df['Шт']*df['Цена']
-print(df)
-df.to_excel('test2.xlsx')
+# print(df)
+# df.to_excel('test2.xlsx')
+# df1 = df[['Год', 'Итого', 'Товар']]
+# df1 = df.loc[10:15]
+# df1 = df.head(5)
+# df1 = df.tail(5)
+# df1 = df[(df['Итого'] > 1000) & (df['Цена'] < 100)]
+# df1 = df.iloc[10:20]
+# print(df.columns)
+# print(df.index)
+# df.set_index()
+# print(df[df['Товар'].isin(['A', 'B'])][['Год', 'Итого']])
+# print(df.sort_values('Итого', ascending=False).head(4))
+# df1 = df.groupby('Товар').Итого.sum()
+# df1 = df['Итого'].max()
+# print(df1)
+# print(df[df['Итого'] == df['Итого'].max()])
+# for i in range(len(df)):
+#     for j in df.columns:
+#         print(df.loc[i, j], end=' ')
+#     print()
+# me = df['Цена'].mean()
+# print(df[df['Цена'] < me])
+# mi = []
+# for i in df.columns:
+#     mi.append(df[i].min)
+# mi_nu
+print(df.groupby('Год').Итого.sum().max())
