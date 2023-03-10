6fdbcdbb6a7ac0a821148a11952bf3bd6d8a4ca9
gilmanov_aleksei <jiexa34pyc@mail.ru>
2023-03-10 19:27:11 +0300

    Lesson 18

--- Testing_Zone/Test_20/Test_20-5.py
+++ Testing_Zone/Test_20/Test_20-5.py
@@ -1,2 +1,17 @@
 #! /usr/bin/python
 
+import pandas as pd
+import random
+
+df = pd.DataFrame(columns=['Год', 'Товар', 'Шт', 'Цена'], index=range(20))
+
+x = 0
+for i in range(2001, 2006):
+    for j in ['A', 'B', 'C']:
+        k = [10, 20, 30, 40, 50][random.randint(0, 4)]
+        m = [100, 50, 30, 20, 5][random.randint(0, 4)]
+        df.iloc[x] = [i, j, k, m]
+        x += 1
+df['Итого'] = df['Шт']*df['Цена']
+print(df)
+df.to_excel('test2.xlsx')
