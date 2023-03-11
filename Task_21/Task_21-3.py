0035382540bf1f7396b4bc68a53329ddb5f89dfd
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-12 00:13:44 +0300

    Lesson 21

--- Task_21/Task_21-3.py
+++ Task_21/Task_21-3.py
@@ -1,4 +1,24 @@
 #! /usr/bin/python
 
 
+# Для создания таблицы "book" с указанными полями в PostgreSQL,
+# необходимо выполнить следующий SQL-запрос:
 
+# CREATE TABLE book (
+#   book_id serial PRIMARY KEY,
+#   book_title text,
+#   book_author text,
+#   publisher_id integer
+# );
+
+# Для вставки нескольких книг, выполните SQL-запрос типа INSERT:
+#
+# INSERT INTO book (book_title, book_author, publisher_id) VALUES
+#     ('1984', 'Джордж Оруэлл', 1),
+#     ('Мастер и Маргарита', 'Михаил Булгаков', 2),
+#     ('Преступление и наказание', 'Федор Достоевский', 3);
+# Здесь мы добавляем три книги разных авторов в таблицу "book".
+# Чтобы выбрать все книги автора "Джордж Оруэлл", выполните SQL-запрос типа SELECT:
+# SELECT * FROM book WHERE book_author = 'Джордж Оруэлл';
+# Аналогично, чтобы выбрать книги с определенным названием, выполните запрос типа SELECT:
+# SELECT * FROM book WHERE book_title = 'Преступление и наказание';
