f47c63d1e1c5cb65854a2ad2f7c5f2b0b523e2b1
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-14 01:11:52 +0300

    Lesson 22

--- Task_22/Task_22-3.py
+++ Task_22/Task_22-3.py
@@ -1,1 +1,20 @@
 #! /usr/bin/python
+
+# Задача 22-3
+# В Python существуют ключевые слова, которые нельзя использовать для названия
+# переменных, функций и классов. Для получения списка ключевых слов можно
+# воспользоваться атрибутом kwlist из модуля keyword.
+# Приведенный ниже код:
+
+# import keyword
+# print(keyword.kwlist)
+# выодит:
+
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def',
+# 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+# 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# Напишите программу, которая принимаетстроку текстаи заменяет в ней всеключевые слова <kw>
+
+# import keyword
+# print(keyword.kwlist)
