bad588631351a2ce6ebd8715367c14b44c2a1c32
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-14 01:44:00 +0300

    Lesson 22

--- Task_22/Task_22-1.py
+++ Task_22/Task_22-1.py
@@ -2,3 +2,6 @@
 # Задача 22-1
 # Вывести содержимое таблицы book, при этом авторов
 # отсортировать по возрастанию, а цены книг по убыванию.
+
+# SELECT author, price FROM book1
+# ORDER BY price DESC, author ASC;
