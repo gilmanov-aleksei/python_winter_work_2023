31266971b192f86dc217cb47f7ab71bc63e5e8d0
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-15 02:27:10 +0300

    Lesson 22

--- Task_22/Task_22-3-3.py
+++ Task_22/Task_22-3-3.py
@@ -9,15 +9,16 @@ from flashtext.keyword import KeywordProcessor
 def replace_words(string):
     # Получаем список всех ключевых слов Python
     kw_list = keyword.kwlist
+
     keyword_processor = KeywordProcessor()
     # Цикл, перебираем все ключевые слова
     for word in kw_list:
-        # Добавляем каждому ключевому слову замену на <kw>
+        # Добавляем ключевые слова в словарь для замены на <kw>
         keyword_processor.add_keyword(word, '<kw>')
     # Заменяем в строке все ключеве на <kw>
     new_string = keyword_processor.replace_keywords(string)
     return new_string
 
 
-text = 'if x == 2: and  as assert print("x is 2") else print("x is not 2")'
+text = 'if x == 2: and  as assert print("x is 2") else: print("x is not 2")'
 print(replace_words(text))
