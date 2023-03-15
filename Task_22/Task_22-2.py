1796c515aa39c90b56f3109a2a1ba295482cec5e
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-15 03:05:18 +0300

    Lesson 22

--- Task_22/Task_22-2.py
+++ Task_22/Task_22-2.py
@@ -7,3 +7,27 @@
 # Каждый кортеж (a,b) показывает, что вершина a соединена с вершиной b.
 # По определению в дереве невозможны циклы.
 # Найти максимальную длину от вержины (1) до конечной вершины.
+
+
+tree = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (6, 7), (17, 8)]
+
+# Создаем словарь, который будет хранить дочерние вершины каждой вершины
+children = {}
+for edge in tree:
+    parent, child = edge
+    if parent not in children:
+        children[parent] = []
+    children[parent].append(child)
+
+
+# Рекурсивно находим максимальную глубину дерева
+def find_max_depth(node):
+    if node not in children:
+        return 0
+    depths = [find_max_depth(child) for child in children[node]]
+    return max(depths) + 1
+
+
+max_depth = find_max_depth(1)  # Начинаем с корня
+
+print("Максимальная длина пути от корня до листа: ", max_depth)
