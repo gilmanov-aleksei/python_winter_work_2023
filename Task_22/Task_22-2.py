2f3473b619f29045f06d2b22ebceb6f6677a4e21
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-15 18:17:36 +0300

    Lesson 22

--- Task_22/Task_22-2.py
+++ Task_22/Task_22-2.py
@@ -11,24 +11,29 @@
 
 
 # Рекурсивно находим максимальную глубину дерева
-def find_max_depth(node, childrens):
-    if node not in childrens:
-        return 0
-    depths = [find_max_depth(child, childrens) for child in childrens[node]]
-    return max(depths) + 1
-
-
-def dict_child(tree):
-    # Создаем словарь, который будет хранить дочерние вершины каждой вершины
-    children = {}
-    for edge in tree:
-        parent, child = edge
-        if parent not in children:
-            children[parent] = []
-        children[parent].append(child)
-    max_depth = find_max_depth(1, children)
-    return max_depth
-
-
+# def find_max_depth(node, childrens):
+#     if node not in childrens:
+#         return 0
+#     depths = [find_max_depth(child, childrens) for child in childrens[node]]
+#     return max(depths) + 1
+#
+#
+# def dict_child(tree):
+#     # Создаем словарь, который будет хранить дочерние вершины каждой вершины
+#     children = {}
+#     for edge in tree:
+#         parent, child = edge
+#         if parent not in children:
+#             children[parent] = []
+#         children[parent].append(child)
+#     max_depth = find_max_depth(1, children)
+#     return max_depth
 bin_tree = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (6, 7), (7, 8), (8, 9)]
-print("Максимальная длина от вершины 1 до конечной вершины: ", dict_child(bin_tree))
+
+# print("Максимальная длина от вершины 1 до конечной вершины: ", dict_child(bin_tree))
+
+d = {}
+for i, j in bin_tree:
+    d[j] = d.get(i, 0) + 1
+print(d)
+print("Максимальная длина от вершины 1 до конечной вершины: ", d[j])
