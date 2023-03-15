db85119c3cee0328a06ded2f97b0c8173f96d321
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-15 22:31:53 +0300

    Lesson 22

--- Task_22/Task_22-2.py
+++ Task_22/Task_22-2.py
@@ -33,7 +33,7 @@ bin_tree = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (6, 7), (7, 8), (8, 9)]
 # print("Максимальная длина от вершины 1 до конечной вершины: ", dict_child(bin_tree))
 
 d = {}
-for i, j in bin_tree:
+for i, j in sorted(bin_tree):
     d[j] = d.get(i, 0) + 1
-print(d)
+print(max(d.values())+1)
 print("Максимальная длина от вершины 1 до конечной вершины: ", d[j])
