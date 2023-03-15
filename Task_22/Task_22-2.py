7ea9e61bff7ba561e057b8fc6eff1dd2c86f87e7
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-15 16:02:15 +0300

    Lesson 22

--- Task_22/Task_22-2.py
+++ Task_22/Task_22-2.py
@@ -1,32 +1,35 @@
 #! /usr/bin/python
 
+
 # Задача 22-2
 # Дана структура типа бинарное дерево. Все вершины пронумерованы от 1 до n.
 # Дерево задано в виде списка кортежей:
 # [(1,2),(1,3),(2,4),(2,5),(3,6),(6,7),(7,8)].
 # Каждый кортеж (a,b) показывает, что вершина a соединена с вершиной b.
 # По определению в дереве невозможны циклы.
-# Найти максимальную длину от вержины (1) до конечной вершины.
+# Найти максимальную длину от вершины (1) до конечной вершины.
 
 
 # Рекурсивно находим максимальную глубину дерева
-def find_max_depth(node):
-    if node not in children:
+def find_max_depth(node, childrens):
+    if node not in childrens:
         return 0
-    depths = [find_max_depth(child) for child in children[node]]
+    depths = [find_max_depth(child, childrens) for child in childrens[node]]
     return max(depths) + 1
 
 
-tree = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (6, 7), (7, 8)]
+def dict_child(tree):
+    # Создаем словарь, который будет хранить дочерние вершины каждой вершины
+    children = {}
+    for edge in tree:
+        parent, child = edge
+        if parent not in children:
+            children[parent] = []
+        children[parent].append(child)
+    max_depth = find_max_depth(1, children)
+    return max_depth
 
-# Создаем словарь, который будет хранить дочерние вершины каждой вершины
-children = {}
-for edge in tree:
-    parent, child = edge
-    if parent not in children:
-        children[parent] = []
-    children[parent].append(child)
 
-max_depth = find_max_depth(1)  # Начинаем с корня
-
-print("Максимальная длина пути от корня до листа: ", max_depth)
+bin_tree = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (6, 7), (7, 8), (8, 9)]
+max_d = dict_child(bin_tree)
+print("Максимальная длина от вершины 1 до конечной вершины: ", max_d)
