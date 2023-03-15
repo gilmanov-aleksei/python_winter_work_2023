b818235e8c496dfa2d04cea01249f2c22d3de6d5
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-16 02:33:38 +0300

    Lesson 23

--- Task_23/Task_23-1.py
+++ Task_23/Task_23-1.py
@@ -6,19 +6,41 @@
 # наибольшим палиндромом является 6 (подстрока 'ccddcc')
 
 def find_long_palindrome(txt):
-    max_len = []
+    max_len = 0
+    word = ''
+    dct = {}
     for i in range(len(txt)):
         for j in range(len(txt), i, -1):
-            if len(max_len) > 0 and len(max_len[0]) >= j - i:
-                break
-            elif txt[i:j] == txt[i:j][::-1]:
-                max_len.append(txt[i:j])
-    max_len = max_len.sort()
-    return max_len
+            x = txt[i:j]
+            if x == x[::-1]:
+                if max_len <= len(x):
+                    max_len = len(x)
+                    dct[x] = dct.get(x, len(x))
+    for key, value in dct.items():
+        if value == max_len:
+            word += key + ' '
+    return word, max_len
 
 
-str = 'aababcacdedcaceeefffeeeklmnonmlk'
-flp = find_long_palindrome(str)
-print(flp)  # ['aceeffea', 'mnonm', 'klk']
+string = 'aababcacdedcaceeefffeeeklmonmlka1s2d3d2s1'
+flp = find_long_palindrome(string)
 
+print("Палиндромы:", flp[0])
+print("Длина полиндрома:", flp[1])
 
+# Рабочий вариант
+# def find_long_palindrome(txt):
+#     max_len = 0
+#     word = []
+#     for i in range(len(txt)):
+#         for j in range(len(txt), i, -1):
+#             x = txt[i:j]
+#             if x == x[::-1]:
+#                 if max_len <= len(x):
+#                     max_len = len(x)
+#                     word.append(x)
+#     word = word[::-1]
+#     for w in word:
+#         if len(w) > len(w[-1]):
+#             word.pop()
+#     return word, max_len
