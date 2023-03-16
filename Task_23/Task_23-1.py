ffa368728cb17f0d09b15d3e7b1672a2e90ecc89
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-16 12:01:50 +0300

    Lesson 22

--- Task_23/Task_23-1.py
+++ Task_23/Task_23-1.py
@@ -36,20 +36,3 @@ flp = find_long_palindrome(string)
 # Результат выводим на экран
 print("Палиндром:", flp[0])
 print("Длина полиндрома:", flp[1])
-
-# Рабочий вариант со списком
-# def find_long_palindrome(txt):
-#     max_len = 0
-#     word = []
-#     for i in range(len(txt)):
-#         for j in range(len(txt), i, -1):
-#             x = txt[i:j]
-#             if x == x[::-1]:
-#                 if max_len <= len(x):
-#                     max_len = len(x)
-#                     word.append(x)
-#     word = word[::-1]
-#     for w in word:
-#         if len(w) > len(w[-1]):
-#             word.pop()
-#     return word, max_len
