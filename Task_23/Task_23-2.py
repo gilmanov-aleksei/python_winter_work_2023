295653d9d3074448dee7ca4d4c3de01c79ced6bc
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-16 03:38:53 +0300

    Lesson 23

--- Task_23/Task_23-2.py
+++ Task_23/Task_23-2.py
@@ -6,3 +6,23 @@
 # Нарисуйте график колличества книги цен.
 # Подсказка: используйте matplotlib и функцию df.plot()
 
+# Программа не начата и не готова.
+
+import psycopg2
+import pandas as ps
+import matplotlib.pyplot as plt
+
+# Данные для графика
+x = [1, 2, 3, 4, 5]
+y = [3, 6, 1, 8, 2]
+
+# Создаем ось и добавляем данные на график
+plt.plot(x, y)
+
+# Добавляем подписи осей и заголовок
+plt.xlabel('Books')
+plt.ylabel('Price')
+plt.title('Books and Price')
+
+# Отображаем график
+plt.show()
