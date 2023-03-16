c11ba24154685dbb3d222daa52ec6c3d1b301f3a
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-16 03:45:41 +0300

    Lesson 23

--- Task_23/Task_23-2.py
+++ Task_23/Task_23-2.py
@@ -6,11 +6,31 @@
 # Нарисуйте график колличества книги цен.
 # Подсказка: используйте matplotlib и функцию df.plot()
 
-# Программа не начата и не готова.
+# Программане готова.
 
 import psycopg2
-import pandas as ps
+import pandas as pd
 import matplotlib.pyplot as plt
+
+con = psycopg2.connect(
+    database="postgres",
+    user="postgres",
+    password="12345678",
+    host="127.0.0.1",
+    port="5432"
+)
+cur = con.cursor()
+
+# cur.execute("INSERT INTO pupil VALUES (2, 'Вася', 1)")
+# cur.execute('SELECT * FROM pupil')
+# d = {'id': [], 'name': [], 'result': []}
+# for i in cur.fetchall():
+#     print(i)
+#     d['id'].append(i[0])
+#     d['name'].append(i[1])
+#     d['result'].append(i[2])
+# # con.commit()
+# con.close()
 
 # Данные для графика
 x = [1, 2, 3, 4, 5]
