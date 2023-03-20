e01eabf95bead98d0b0decd7e2d661665909a7b3
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-20 15:45:28 +0300

    Lesson 24

--- Task_24/Task_24-2.py
+++ Task_24/Task_24-2.py
@@ -5,3 +5,22 @@
 # минимальное количество книг на складе (в таблице book).
 # Используйте для этого View.
 
+import psycopg2
+
+# Подключаемся к базе Postgres
+con = psycopg2.connect(
+    database="postgres",
+    user="postgres",
+    password="12345678",
+    host="127.0.0.1",
+    port="5432"
+)
+cur = con.cursor()
+# Вызываем таблицу book
+cur.execute('SELECT * FROM book')
+# Копируем таблицу из базы Postgres в переменную
+rows = cur.fetchall()
+# Добавляем названия колонок для DataFrame
+
+cur.close()
+con.close()
