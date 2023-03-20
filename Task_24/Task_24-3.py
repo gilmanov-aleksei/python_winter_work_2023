8f3238a183e9f8c2ab907f6223112f36574b5ea4
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-20 10:02:18 +0300

    Lesson 24

--- Task_24/Task_24-3.py
+++ Task_24/Task_24-3.py
@@ -13,13 +13,20 @@
 # "(()) (( ()() ) () )" => true
 
 def string_validation(string):
+    # Переменная для подсчёта скобок
     i = 0
+    # Цикл по строке
     for char in string:
+        # Проверяем символ из строки,
+        # если окрывающая скобка,
         if char == '(':
+            # то прибавлем переменную на 1
             i += 1
+        # если закрывающая стрка, то убавляем на 1
         elif char == ')':
+            #
             if not i:
-                return i == 1
+                return False
             else:
                 i -= 1
     return i == 0
@@ -32,5 +39,7 @@ print(string_validation(str))
 str = "("  # => false
 print(string_validation(str))
 str = "(()) (( ()() ) () )"  # => true
+print(string_validation(str))
+str = ")("  # => true
 print(string_validation(str))
 
