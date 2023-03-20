229a60611782a99aa13b79fc04719de1e71c3f10
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-20 20:46:11 +0300

    TestFile

--- Testing_Zone/Test_25/Test_25-1.py
+++ Testing_Zone/Test_25/Test_25-1.py
@@ -52,8 +52,8 @@ class MainWindows(QMainWindow):
         button = QPushButton("Press Me!")
 
         button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked())
-        button.clicked.connect(self.the_button_was_clicked())
+        button.clicked.connect(self.the_button_was_clicked)
+        button.toggled.connect(self.the_button_was_toggled)
         self.setCentralWidget(button)
 
     def the_button_was_clicked(self):
@@ -62,10 +62,10 @@ class MainWindows(QMainWindow):
         self.setWindowTitle(f"Clicked! {self.count}")
 
     def the_button_was_toggled(self, checked):
-        print("Checket?", checked)
+        print("Checked?", checked)
 
 
 app = QApplication(sys.argv)
-window = QMainWindow()
+window = MainWindows()
 window.show()
-app.exec()
+sys.exit(app.exec())
