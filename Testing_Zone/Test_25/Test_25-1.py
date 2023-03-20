001234d51d0967ca7689fbad7c1c0640f0589273
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-20 20:02:45 +0300

    TestFileush

--- Testing_Zone/Test_25/Test_25-1.py
+++ Testing_Zone/Test_25/Test_25-1.py
@@ -1,2 +1,37 @@
 #! /usr/bin/python
 
+# from PyQt6.QtWidgets import QApplication, QWidget
+# # import sys
+#
+# app = QApplication([])
+# window = QWidget()
+# window.show()
+# app.exec()
+
+# from PyQt6.QtWidgets import QApplication, QPushButton
+# import sys
+#
+# app = QApplication(sys.argv)
+# window = QPushButton("Push Me")
+# window.show()
+# app.exec()
+
+# from PyQt6.QtWidgets import QApplication, QMainWindow
+# import sys
+#
+# app = QApplication(sys.argv)
+# window = QMainWindow()
+# window.show()
+# app.exec()
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+import sys
+
+class MainWindows(QMainWindow):
+    def __init__(self):
+        super(MainWindows, self).__init__()
+        self.setWindowTitle("My App")
+app = QApplication(sys.argv)
+window = MainWindows()
+window.show()
+app.exec()
