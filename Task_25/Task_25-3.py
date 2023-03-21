c177ca8eccaa71a238f30a0722ffbe8861729c8d
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-21 12:42:56 +0300

    Lesson 25

---
+++ Task_25/Task_25-3.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+
+# Задача 25-3
+
+# Напишите функцию, которой на вход подается строка, содержащая
+# последовательность слов (которые могут включать буквы верхнего и нижнего
+# регистра). На выходе должна получиться строка в CamelStyle
+# Например, "camel case word" => CamelCaseWord
+
+# Вариант 1
+def camel_style(string):
+    # Пустая строка для сбора слов
+    new_str = ""
+    # Все слова начинаются с заглавной буквы
+    # и продолжаются строчными, методом title()
+    # Методом split() переводим строку в список, через разделитель
+    words = string.title().split(" ")
+    # Цикл по списку
+    for word in words:
+        # Каждое слово прибавляем в новую строку
+        new_str += word
+    return new_str
+
+
+text1 = "caMel caSe woRd"
+print(camel_style(text1))
+
+# Вариант 2
+def to_camel_style(string):
+    # используем генератор списков для создания списка, с помощью метода capitalize(),
+    # переводя первую буквы в верхний регистр, а остальные в нижний.
+    # и затем объединяем их с помощью метода join()
+    return ''.join(word.capitalize() for word in string.split())
+
+
+text2 = "caMel cASe wORd"
+print(to_camel_style(text2))
