fa2f14c35257bb6487d308bb1743ab21a245513c
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-22 19:13:24 +0300

    TestFile

--- Testing_Zone/Test_26/Test_26-1.py
+++ Testing_Zone/Test_26/Test_26-1.py
@@ -1,1 +1,18 @@
 #! /usr/bin/python
+
+# demo_list = [5, 4, 4, 6, 8, 12, 12, 1, 5]
+# lst = []
+# dct = {}
+# unique_list = list(set(demo_list))
+# print(unique_list)
+#
+# for i in demo_list:
+#     if i not in lst:
+#         lst.append(i)
+# print(lst)
+# lst = []
+# for n in demo_list:
+#     dct[n] = dct.get(n, 0) + 1
+#     if dct[n] > 1:
+#         lst.append(n)
+# print(lst)
