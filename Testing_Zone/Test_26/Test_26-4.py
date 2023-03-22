10c54b5023a3437ed09d1014166da09fee20b5cf
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-22 21:42:02 +0300

    TestFile

--- Testing_Zone/Test_26/Test_26-4.py
+++ Testing_Zone/Test_26/Test_26-4.py
@@ -1,1 +1,43 @@
 #! /usr/bin/python
+
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMdiArea, QMdiSubWindow, QTextEdit
+from PyQt6.QtGui import QAction
+import sys
+
+
+class MDIWindow(QMainWindow):
+    counter = 0
+
+    def __init__(self):
+        super().__init__()
+
+        self.mdi = QMdiArea()
+        self.setCentralWidget(self.mdi)
+        bar = self.menuBar()
+        file = bar.addMenu("File")
+        file.addAction("New")
+        file.addAction("Cascade")
+        file.addAction("Tiled")
+        file.triggered(QAction).connect(self.WindowTrig)
+        self.setWindowTitle("MDI Application")
+
+    def WindowTrig(self, p):
+        if p.text() == "New":
+            MDIWindow.count = MDIWindow.count + 1
+            sub = QMdiSubWindow()
+            sub.setWidget(QTextEdit)
+            sub.setWindowTitle("Sub" + str(MDIWindow.count))
+            self.mdi.addSubWindow(sub)
+            sub.show()
+        if p.texty() == "Cascade":
+            self.mdi.cascadeSubWindows()
+        if p.text() == "Tiled":
+            self.mdi.tileSubWindows()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mdiwindow = MDIWindow()
+    mdiwindow.show()
+    app.exec()
--- Testing_Zone/Test_26/Test_26-5.py
+++ Testing_Zone/Test_26/Test_26-5.py
@@ -1,1 +1,54 @@
 #! /usr/bin/python
+
+# class Anyclass:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.__y = y
+#
+# any = Anyclass(123, 456)
+# print(any.x)
+# print(any.__y)
+# print(any._Anyclass__y)
+
+# class Person:
+#     def __init__(self, name):
+#         self.__name = name
+#         self.__age = 1
+#
+#     def set_age(self, age):
+#         if 1 < age < 10:
+#             self.__age = age
+#         else:
+#             print("Недопусимый возраст")
+#
+#     def get_age(self):
+#         return self.__age
+#
+#     def get_name(self):
+#         return self.__name
+#
+#     def display_info(self):
+#         print(f"Имя: {self.__name} Возраст: {self.__age}")
+#
+# tom = Person("Tom")
+# tom.display_info()
+# tom.set_age(25)
+# tom.display_info()
+
+class Foo(object):
+    bar = True
+Foo1 = type('Foo1', (), {'bar': True})
+Foo1Child = type('Foo1Child', (Foo1,), {})
+
+def echo_bar(self):
+    print(self.bar)
+
+FooChild = type('FooChild', (Foo,), {'echo_bar': echo_bar})
+
+print(hasattr(FooChild, 'echo_bar'))
+
+my_foo = FooChild()
+my_foo.echo_bar()
+my_foo.bar = 123
+my_foo.echo_bar()
+print(FooChild.__dict__)
