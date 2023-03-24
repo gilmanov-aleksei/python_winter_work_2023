aab3a5aed542bd5d69cbaeac89635874129341f4
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-24 16:01:29 +0300

    Lesson 26

---
+++ Task_26/Task_26-1-2.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+
+# Задача 26-1
+
+# Напишите функцию, которая сравнивает две строки и выдает True,
+# если между ними есть не более, чем 1 разница в буквах, и False во
+# всех остальных случаях. Если две строки равны, то True.
+# Например:
+# 'abc' и 'abc' - True, 'abc' и abcd ' - True,
+# ' bc' и 'abc' - True, 'axc' и 'abc' - True
+# 'abc' и 'acb' - False, 'abc' и 'a' - False, '' и '   ' - False
+
+def compare_str(str1, str2):
+    if str1 == str2:
+        return True
+
+    if abs(len(str1) - len(str2)) > 1:
+        return False
+
+    if len(str1) < len(str2):
+        str1, str2 = str2, str1
+
+    for i in range(len(str2)):
+        if str1[i] != str2[i]:
+            if len(str1) == len(str2):
+                return str1[i + 1:] == str2[i + 1:]
+            else:
+                return str1[i + 1:] == str2[i:]
+
+    return True
+
+
+print(compare_str('abc', 'abc'))  # True
+print(compare_str('abc', 'abcd'))  # True
+print(compare_str('bc', 'abc'))  # True
+print(compare_str('axc', 'abc'))  # True
+print(compare_str('abc', 'acb'))  # False
+print(compare_str('abc', 'a'))  # False
+print(compare_str(' ', '   '))  # False
