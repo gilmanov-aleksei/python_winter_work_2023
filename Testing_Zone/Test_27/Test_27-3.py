b29f710e4b92781466b5f84ff7e74b753f94000f
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-24 21:28:56 +0300

    TestFile

--- Testing_Zone/Test_27/Test_27-3.py
+++ Testing_Zone/Test_27/Test_27-3.py
@@ -1,2 +1,14 @@
 #! /usr/bin/python
 
+class Sinleton:
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Sinleton, cls).__new__(cls)
+        return cls.instance
+
+
+s1 = Sinleton()
+s2 = Sinleton()
+print(s1 is s2)
+s1.x = 123
+print(s2.x)
--- Testing_Zone/Test_27/Test_27-4.py
+++ Testing_Zone/Test_27/Test_27-4.py
@@ -1,2 +1,24 @@
 #! /usr/bin/python
 
+lst = [(0, 0), (1, 1), (2, 2), (1, 2), (0, 2), (2, 1), (2, 0)]
+pname = 'Point'
+xyz = type(pname, (), {'x': 0, 'y': 0})
+for i in lst:
+    s = 'p'+str(i[0])+str(i[1])
+    s = xyz()
+    s.x = i[0]
+    s.y = i[1]
+
+
+#
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def __str__(self):
+#         return f"Вектор с координатами ({self.x}, {self.y})"
+#
+#     def __repr__(self):
+#         return f"Vector ({self.x}, {self.y})"
