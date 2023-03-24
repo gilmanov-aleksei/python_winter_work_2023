a0599fc8dfd514dd93e2403006657448cb48a64b
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-25 01:16:23 +0300

    Lesson 27

--- Task_27/Task_27-3.py
+++ Task_27/Task_27-3.py
@@ -10,3 +10,44 @@
 # ["x", "y", ["z"]] -->4
 # [1, 2, [3, 4, [5]]] -->7
 
+def count_elm(lst):
+    # Переменная для количества элементов
+    count = 0
+    # Цикл для перебора всех элементов списка
+    for x in lst:
+        # Если элемент является списком,
+        if type(x) == list:
+            # увеличиваем переменную на 1
+            count += 1
+            # рекурсия для перебора вложенных списков
+            count += count_elm(x)
+        # Если элемент не является списком,
+        else:
+            # увеличиваем переменную на 1
+            count += 1
+    return count
+
+
+print(count_elm([]), " --> 0")
+print(count_elm([1, 2, 3]), " --> 3")
+print(count_elm(["x", "y", ["z"]]), "--> 4")
+print(count_elm([1, 2, [3, 4, [5]]]), " --> 7")
+print(count_elm([1, 2, [3, 4, [5, 6, [7]]]]), " --> 10")
+
+
+def count_elements(lst):
+    count = 0
+    for item in lst:
+        if isinstance(item, list):
+            count += 1
+            count += count_elements(item)
+        else:
+            count += 1
+    return count
+
+
+print("Вариант из интернета")
+print(count_elements([]))  # 0
+print(count_elements([1, 2, 3]))  # 3
+print(count_elements(["x", "y", ["z"]]))  # 4
+print(count_elements([1, 2, [3, 4, [5]]]))  # 7
