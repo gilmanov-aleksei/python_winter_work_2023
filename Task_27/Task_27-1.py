7249cbc4e7b70085592103e113c3e5da6cf24029
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-25 01:21:27 +0300

    Lesson 27

--- Task_27/Task_27-1.py
+++ Task_27/Task_27-1.py
@@ -11,21 +11,15 @@
 # 1 2 2 2 1
 # 1 1 1 1 1
 
-n = 1
-while n > 0:
+
+while True:
     n = int(input("Введите число n от 1 до 18, 0 - Выход: "))
-    if n != 0:
-        if n > 18:
+    if n == 0:
+        break
+    else:
+        if n > 18 or n < 0:
             continue
         else:
-            print("Вариант 1 - без матрицы\n")
-            for i in range(1, n + 1):
-                for j in range(1, n + 1):
-                    val = min(i, j, n - i + 1, n - j + 1)
-                    print(val, end=' ')
-                print()
-            print()
-            print("Вариант 2 - с матрицей\n")
             # Создаем матрицу размером n на n и заполняем ее нулями
             matrix = [[0] * n for x in range(n)]
             # Заполняем матрицу цифрами
