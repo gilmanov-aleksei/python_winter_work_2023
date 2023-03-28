77c464e41f4c1f1913a5eeba52c98e3f358df958
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-29 00:47:22 +0300

    Lesson 28

--- Task_28/Task_28-1.py
+++ Task_28/Task_28-1.py
@@ -18,7 +18,46 @@ def find_inversions_num(lst):
                     count += 1
     return count
 
+
+print("Компактый код, который выполняется за время O(n^2)")
 a = [1, 2, 3, 4, 5]
 print(find_inversions_num(a), "[1,2,3,4,5] -> 0")
 a = [5, 4, 3, 2, 1]
 print(find_inversions_num(a), "[5,4,3,2,1] -> 10")
+
+
+def merge_sort(arr, inv_count=0):
+    if len(arr) <= 1:
+        return arr, inv_count
+    mid = len(arr) // 2
+    left, inv_count = merge_sort(arr[:mid], inv_count)
+    right, inv_count = merge_sort(arr[mid:], inv_count)
+
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+            inv_count += len(left) - i
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result, inv_count
+
+
+def find_inversion_num(lst):
+    _, inv_count = merge_sort(lst)
+    return inv_count
+
+
+print("Быстрый код, который выполняется за время O(n * log(n))")
+a = [1, 2, 3, 4, 5]
+print(find_inversion_num(a), "[1,2,3,4,5] -> 0")
+a = [5, 4, 3, 2, 1]
+print(find_inversion_num(a), "[5,4,3,2,1] -> 10")
