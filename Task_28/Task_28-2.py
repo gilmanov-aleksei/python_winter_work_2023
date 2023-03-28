651c44d26593d6b8c5269e894073a50040b78c5d
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-28 23:19:32 +0300

    Lesson 28

--- Task_28/Task_28-2.py
+++ Task_28/Task_28-2.py
@@ -9,22 +9,19 @@
 # abc и xyz - 3
 
 def find_simbol(s1, s2):
-    if s1 == s2:
-        count = 0
-        lens = len(s1)
-        for i in range(len(s1)):
-            if s1[i] != s2[i]:
-                count += 1
-
+    count = 0
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            count += 1
     return count
 
 
 srt1 = "abc"
 srt2 = "abc"
-print(srt1 + " и " + srt2 + " --", find_simbol(srt1, srt2), "  abc и abc - 0")
+print(f"{srt1} и {srt2} - {find_simbol(srt1, srt2)}")
 srt1 = "abc"
 srt2 = "abd"
-print(srt1 + " и " + srt2 + " --", find_simbol(srt1, srt2), "  abc и abd - 1")
+print(f"{srt1} и {srt2} - {find_simbol(srt1, srt2)}")
 srt1 = "abc"
 srt2 = "xyz"
-print(srt1 + " и " + srt2 + " --", find_simbol(srt1, srt2), "  abc и xyz - 3")
+print(f"{srt1} и {srt2} - {find_simbol(srt1, srt2)}")
