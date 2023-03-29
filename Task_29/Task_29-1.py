f387f12a4e734d8e559622e5311abed0feb46b1b
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-30 02:50:56 +0300

    Lesson 29

--- Task_29/Task_29-1.py
+++ Task_29/Task_29-1.py
@@ -7,16 +7,16 @@
 
 
 lst = [11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11]
-print("Искомое число:", "".join(map(str, [num for num in lst if lst.count(num) == 1])))
+print("Исключение:", "".join(map(str, [num for num in lst if lst.count(num) == 1])))
 
 for num in lst:
     if lst.count(num) == 1:
-        print("Искомое число:", num)
+        print("Исключение:", num)
 
 dct = {}
 for i in lst:
     dct[i] = dct.get(i, 0) + 1
-print("Искомое число:", *[k for k, v in dct.items() if v == 1])
+print("Исключение:", *[k for k, v in dct.items() if v == 1])
 # for k, v in dct.items():
 #     if v == 1:
 #         print("Искомое число:", k)
