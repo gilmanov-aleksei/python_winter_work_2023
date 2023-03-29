2fa7a8808c2fec68a7071d89331432c2f82cfd24
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-29 19:40:43 +0300

    TestFile

--- Testing_Zone/Test_29/Test_29-1.py
+++ Testing_Zone/Test_29/Test_29-1.py
@@ -1,2 +1,27 @@
 #! /usr/bin/python
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next_node = None
+
+
+a = Node(1)
+b = Node(22)
+c = Node(35)
+d = Node(7)
+e = Node(15)
+a.next_node = b
+b.next_node = c
+c.next_node = d
+d.next_node = e
+
+ma = a.value
+print(ma)
+x = a
+while x.next_node != None:
+    if x.value > ma:
+        ma = x.value
+        print(ma)
+    x = x.next_node
+print(ma)
--- Testing_Zone/Test_29/Test_29-2.py
+++ Testing_Zone/Test_29/Test_29-2.py
@@ -1,2 +1,8 @@
 #! /usr/bin/python
 
+# s = "Используя стек, напечатайте этот список в обратном порядке"
+# lst = list(s)
+# while True:
+#     print(lst.pop(), end='')
+#
+
