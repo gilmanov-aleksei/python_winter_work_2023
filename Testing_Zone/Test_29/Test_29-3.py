cb6b2154813a685e22969ddf7600283f0ee99463
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-29 20:54:24 +0300

    TestFile

--- Testing_Zone/Test_29/Test_29-3.py
+++ Testing_Zone/Test_29/Test_29-3.py
@@ -3,12 +3,13 @@
 from flask import Flask, render_template
 
 manu = ["Первый", "Второй", "Третий"]
+
 app = Flask(__name__)
 
 
 @app.route('/index')
 @app.route('/')
-def index():
+def index(menu=None):
     return render_template('index.html', title='Про Flask', menu=menu)
 
 
