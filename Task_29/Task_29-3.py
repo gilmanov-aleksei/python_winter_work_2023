7c84e03944ee9ec6de38024be1d71b97743f49d4
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-29 23:03:43 +0300

    Lesson 29

--- Task_29/Task_29-3.py
+++ Task_29/Task_29-3.py
@@ -1,2 +1,17 @@
 #! /usr/bin/python
 
+# Задача 29-3
+
+# Напишите функцию, которая проверяет, являются ли два слова изоморфными.
+# Два слова изоморфны, если буквами одного слова можно сопоставить (map)
+# буквами другого слова.
+
+# True:
+# CBAABC DEFFED
+# XXX YYY
+# RAMBUNCTIOUSLY THERMODYNAMICS
+
+# False:
+# AB CC
+# XXY XYY
+# ABAB CD
