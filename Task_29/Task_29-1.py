b8394c3f3f0a5d0b65433b3f83796101aa9f9d79
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-30 00:12:48 +0300

    Lesson 29

--- Task_29/Task_29-1.py
+++ Task_29/Task_29-1.py
@@ -7,17 +7,17 @@
 
 
 lst = [11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11]
-print("".join(map(str, [num for num in lst if lst.count(num) == 1])))
+print("Искомое число:", "".join(map(str, [num for num in lst if lst.count(num) == 1])))
 
-# for num in lst:
-#     if lst.count(num) == 1:
-#         print("Искомое число:", num)
-# num for num in lst if lst.count(num) == 1
+for num in lst:
+    if lst.count(num) == 1:
+        print("Искомое число:", num)
 
-# lst = [11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11]
-# dct = {}
-# for elem in lst:
-#     dct[elem] = dct.get(elem, 0) + 1
-# for key, value in dct.items():
-#     if value == 1:
-#         print("Искомое число:", key)
+lst = [11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11]
+dct = {}
+for i in lst:
+    dct[i] = dct.get(i, 0) + 1
+print("Искомое число:", *[k for k, v in dct.items() if v == 1])
+# for k, v in dct.items():
+#     if v == 1:
+#         print("Искомое число:", k)
