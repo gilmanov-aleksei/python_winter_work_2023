6136bf6aa5d3add901d624b078dd1cc32b953626
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-29 20:46:45 +0300

    TestFile

--- Testing_Zone/Test_29/Test_29-3.py
+++ Testing_Zone/Test_29/Test_29-3.py
@@ -1,13 +1,21 @@
 #! /usr/bin/python
 
-from flask import Flask
+from flask import Flask, render_template
 
+manu = ["Первый", "Второй", "Третий"]
 app = Flask(__name__)
 
+
+@app.route('/index')
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html', title='Про Flask', menu=menu)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='О сайте')
 
 
 if __name__ == "__main__":
-    app.route(debug=True)
+    app.run(debug=True)
