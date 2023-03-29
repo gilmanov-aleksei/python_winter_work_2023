3b7e1f8ab0756ab21e0e675225cebb2d9b92be13
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-30 00:06:58 +0300

    Lesson 29

--- Task_29/Task_29-1.py
+++ Task_29/Task_29-1.py
@@ -2,5 +2,22 @@
 
 # Задача 29-1
 
-# Дан список, который слстоит из одинаковых чисел за исключаением одного.
+# Дан список, который состоит из одинаковых чисел за исключаением одного.
 # Найдите это число.
+
+
+lst = [11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11]
+print("".join(map(str, [num for num in lst if lst.count(num) == 1])))
+
+# for num in lst:
+#     if lst.count(num) == 1:
+#         print("Искомое число:", num)
+# num for num in lst if lst.count(num) == 1
+
+# lst = [11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11]
+# dct = {}
+# for elem in lst:
+#     dct[elem] = dct.get(elem, 0) + 1
+# for key, value in dct.items():
+#     if value == 1:
+#         print("Искомое число:", key)
