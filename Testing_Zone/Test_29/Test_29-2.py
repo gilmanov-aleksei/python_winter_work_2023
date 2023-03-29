1c2b41bdd01904ce995e1d6feb1520d972cfff74
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-29 20:21:31 +0300

    TestFile

--- Testing_Zone/Test_29/Test_29-2.py
+++ Testing_Zone/Test_29/Test_29-2.py
@@ -6,3 +6,40 @@
 #     print(lst.pop(), end='')
 #
 
+# s = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+#      [11, 12, 13, 14, 15]]
+s = []
+
+
+def push_s(i):
+    if not s:
+        s.append([])
+        s[-1].append(i)
+    else:
+        if len(s[-1]) < 10:
+            s[-1].append(i)
+        else:
+            s.append([])
+            s[-1].append(i)
+
+    return
+
+
+def pop_s():
+    if not s:
+        return "Тарелок нет"
+    else:
+        if len(s[-1]) > 1:
+            return s[-1].pop()
+        else:
+            x = s[-1].pop()
+            s.pop()
+            return x
+
+
+for i in range(1, 15):
+    push_s(i)
+    print(s)
+for _ in range(10):
+    i = pop_s()
+    print(s)
