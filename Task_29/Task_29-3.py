792f38cd07c404880835d1b8ba5a9d77ef308759
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-30 03:35:58 +0300

    Lesson 29

--- Task_29/Task_29-3.py
+++ Task_29/Task_29-3.py
@@ -20,28 +20,29 @@ def isomorphic_words(s1, s2):
     # Если s1 и s2 имеют разную длину, они не могут быть изоморфны
     if len(s1) != len(s2):
         return False
-    # Словарь для сопоставления символов строки s1 со строкой s2.
-    d = {}
-    # Множество для сопоставленных символов.
-    s = set()
+    # Словарь для хранения сопоставления строки s1 со строкой s2.
+    dct = {}
+    # Множество для хранения уже сопоставленных символов.
+    sets = set()
     for i in range(len(s1)):
-        x = s1[i]
-        y = s2[i]
-        if x in d:
-            if d[x] != y:
+        # Если s1[i] уже был раньше
+        if s1[i] in dct:
+            # возвращает False, если вхождение s1[i] находиться в s2[i]
+            if dct[s1[i]] != s2[i]:
                 return False
+        # , если x виден в впервые
         else:
-            if y in s:
+            # возвращает False, если s2[i] уже сопоставлен с каким-либо символом в s1
+            if s2[i] in sets:
                 return False
-
-            d[x] = y
-            s.add(y)
-
+            # Cопоставляет s1 с s2 и помечаем как сопоставленный
+            dct[s1[i]] = s2[i]
+            sets.add(s2[i])
     return True
 
 
 str1 = ["CBAABC", "XXX", "RAMBUNCTIOUSLY", "AB", "XXY", "ABAB"]
-str2 = ["DEFFED", "ABAB", "THERMODYNAMICS", "CC", "XYY", "CD"]
+str2 = ["DEFFED", "YYY", "THERMODYNAMICS", "CC", "XYY", "CD"]
 
 for i in range(len(str1)):
-    print(isomorphic_words(str1[i], str2[i]))
+    print(str1[i], str2[i], "--->", isomorphic_words(str1[i], str2[i]))
