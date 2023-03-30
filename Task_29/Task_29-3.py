180372c8174a03c062ef1384921be92f7390ab66
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-03-30 03:12:00 +0300

    Lesson 29

--- Task_29/Task_29-3.py
+++ Task_29/Task_29-3.py
@@ -5,19 +5,39 @@
 # Напишите функцию, которая проверяет, являются ли два слова изоморфными.
 # Два слова изоморфны, если буквами одного слова можно сопоставить (map)
 # буквами другого слова.
-
+#
 # True:
 # CBAABC DEFFED
-# XXX c
+# XXX YYY
 # RAMBUNCTIOUSLY THERMODYNAMICS
-
+#
 # False:
 # AB CC
 # XXY XYY
 # ABAB CD
 
 def isomorphic_words(s1, s2):
-    return s1 + "-" + s2
+    # Если s1 и s2 имеют разную длину, они не могут быть изоморфны
+    if len(s1) != len(s2):
+        return False
+    # Словарь для сопоставления символов строки s1 со строкой s2.
+    d = {}
+    # Множество для сопоставленных символов.
+    s = set()
+    for i in range(len(s1)):
+        x = s1[i]
+        y = s2[i]
+        if x in d:
+            if d[x] != y:
+                return False
+        else:
+            if y in s:
+                return False
+
+            d[x] = y
+            s.add(y)
+
+    return True
 
 
 str1 = ["CBAABC", "XXX", "RAMBUNCTIOUSLY", "AB", "XXY", "ABAB"]
