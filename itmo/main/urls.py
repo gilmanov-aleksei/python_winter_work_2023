31be1db55a86b1e7fa7cc8e5a826ccd140d4f8af
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-04-02 02:35:58 +0300

    Diplom

--- itmo/main/urls.py
+++ itmo/main/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index),
-	path('index', views.about),
-	path('about', views.about),
+	path('', views.index, name='home'),
+	path('about', views.about, name='about'),
 	path('help', views.help)
 ]
