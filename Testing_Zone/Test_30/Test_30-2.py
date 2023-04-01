b855f8db45fee0e172bbad022981e8a9a5211b6a
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-04-01 22:22:39 +0300

    TestFile

--- Testing_Zone/Test_30/Test_30-2.py
+++ Testing_Zone/Test_30/Test_30-2.py
@@ -32,23 +32,24 @@ import unittest
 
 
 def cube_area(side):
-    return 6 * side ** 2
+	return 6 * side ** 2
+
 
 class TestCubeArea(unittest.TestCase):
-    def test_cube_area(self):
-        self.assertEqual(cube_area(3), 54)
-        self.assertEqual((0), 0)
-
-    # def test_value(self):
-    #     self.assertRaises(ValueError, cube_area, -1)
-
-    def test_type(self):
-        self.assertRaises(TypeError, cube_area, 'String')
-        # self.assertRaises(TypeError, cube_area, True)
-        self.assertRaises(TypeError, cube_area, [0])
-        self.assertRaises(TypeError, cube_area, (1, 2))
-        self.assertRaises(TypeError, cube_area, {})
+	def test_cube_area(self):
+		self.assertEqual(cube_area(3), 54)
+		self.assertEqual((0), 0)
+	
+	# def test_value(self):
+	#     self.assertRaises(ValueError, cube_area, -1)
+	
+	def test_type(self):
+		self.assertRaises(TypeError, cube_area, 'String')
+		# self.assertRaises(TypeError, cube_area, True)
+		self.assertRaises(TypeError, cube_area, [0])
+		self.assertRaises(TypeError, cube_area, (1, 2))
+		self.assertRaises(TypeError, cube_area, {})
 
 
 if __name__ == '__main__':
-    unittest.main
+	unittest.main
