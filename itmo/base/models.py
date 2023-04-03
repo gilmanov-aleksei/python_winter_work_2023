90acc66286637450de690ecd10a04e3de59aae80
gilmanov-aleksei <jiexa34pyc@mail.ru>
2023-04-03 22:49:47 +0300

    Diplom

--- itmo/base/models.py
+++ itmo/base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Artiles(models.Model):
+class Articles(models.Model):
 	title = models.CharField('Название', max_length=50)
 	anons = models.CharField('Анонс', max_length=250)
 	full_text = models.TextField('Статья')
